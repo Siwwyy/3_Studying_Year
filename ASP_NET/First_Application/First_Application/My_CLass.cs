c865d9b65de77bfb55f9a655f7473c80ae324c61
Damian Andrysiak <andrydamian@gmail.com>
2019-10-08 16:12:33 +0200

    dasdad

--- ASP_NET/First_Application/First_Application/My_CLass.cs
+++ ASP_NET/First_Application/First_Application/My_CLass.cs
@@ -4,12 +4,44 @@ using System.Text;
 
 namespace First_Application
 {
-    class My_CLass
+    class My_Class
     {
         private int a;
-        My_CLass()
-        {
+        private string _firstName;
+        private string _lastName;
 
+        public My_Class(string first, string last)
+        {
+            _firstName = first;
+            _lastName = last;
+        }
+
+        public My_Class(int a)
+        {
+            this.a = a;
+        }
+
+        public string Name => $"{_firstName} {_lastName}";
+        
+
+        private int id;
+
+        public int SetGet   //wlasciwosc
+        {
+            get
+            {
+                return a;
+            }
+
+            set
+            {
+                a = value;
+            }
+        }
+
+        int get_a()
+        {
+            return this.a;
         }
     }
 }
