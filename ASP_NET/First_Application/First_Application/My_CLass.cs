664bcefda0f3d619c9fef3497cfc15942a974568
Damian Andrysiak <andrydamian@gmail.com>
2019-10-09 20:59:01 +0200

    working basic project in C# dot NET

--- ASP_NET/First_Application/First_Application/My_CLass.cs
+++ ASP_NET/First_Application/First_Application/My_CLass.cs
@@ -6,7 +6,92 @@ namespace First_Application
 {
     class My_Class
     {
+        private string Name;
+        private string Surname;
 
+        /*
+            CASE OF CONSTRUCTORS
+        */
+        public My_Class()
+        {
+            this.Name = default;
+            this.Surname = default;
+        }
 
+        public My_Class(string name, string surname)
+        {
+            this.Name = name;
+            this.Surname = surname;
+        }//Parametrized constructor up here
+         ////////////////////////////////////////////
+         ///
+        /*
+            CASE OF SETTERS
+        */
+        public void Set_Name(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public void Set_Surname(string Surname)
+        {
+            this.Surname = Surname;
+        }
+        ////////////////////////////////////////////
+        ///
+        /*
+            CASE OF PROPERTIES
+        */
+        public string Name_Property
+        {
+            set
+            {
+                if (value != " ")
+                {
+                    this.Name = value;
+                }
+                else
+                {
+                    Console.WriteLine("Check the correctness of the entered name!");
+                }
+            }
+            get
+            {
+                return this.Name;
+            }
+        }
+
+        public string Surname_Property
+        {
+            set
+            {
+                if (value != " ")
+                {
+                    this.Surname = value;
+                }
+                else
+                {
+                    Console.WriteLine("Check the correctness of the entered surname!");
+                }
+            }
+            get
+            {
+                return this.Surname;
+            }
+        }
+        ////////////////////////////////////////////
+        ///
+        /*
+           CASE OF GETTERS
+        */
+        public string Get_Name()
+        {
+            return this.Name;
+        }
+        public string Get_Surname()
+        {
+            return this.Surname;
+        }
+        ////////////////////////////////////////////
     }
 }
--- ASP_NET/First_Application/First_Application/Program.cs
+++ ASP_NET/First_Application/First_Application/Program.cs
@@ -6,83 +6,6 @@ namespace First_Application
     {
         static void Main(string[] args)
         {
-            //    Console.WriteLine("Please enter the First Name");
-            //    Console.WriteLine("Please enter the Last Name");
-            //    string LastName = Console.ReadLine();
-            //    Console.WriteLine("Hello {0} {1} !!!", FirstName, LastName);
-            //string my_string = "";
-            //while (true)
-            //{
-            //    my_string = Console.ReadLine();
-            //    if (my_string == "quit")
-            //    {
-            //        break;
-            //    }
-            //    else
-            //    {
-            //        //POPRAW w DOMU!!!!!!
-            //        int length = my_string.Length;
-            //       // if (my_string_heh == "+")
-            //        if (my_string.Contains("+"))
-            //        {
-            //            int index_ = my_string.IndexOf('+');
-            //            string my_string_one = my_string.Substring(0, index_);
-            //         //   string my_string_heh = my_string.Substring(index_, index_- 1);
-            //            string my_string_two = my_string.Substring(index_, (length - 2));
-            //            int value_first = Int32.Parse(my_string_one);
-            //            int value_second = Int32.Parse(my_string_two);
-            //            int result = 0;
-            //            result = (value_first + value_second);
-            //            Console.WriteLine("Result: {0}", result);
-            //        }
-            //        if (my_string.Contains("-"))
-            //        {
-            //            int index_ = my_string.IndexOf('-');
-            //            string my_string_one = my_string.Substring(0, index_);
-            //            //   string my_string_heh = my_string.Substring(index_, index_- 1);
-            //            string my_string_two = my_string.Substring(index_, (length - 2));
-            //            int value_first = Int32.Parse(my_string_one);
-            //            int value_second = Int32.Parse(my_string_two);
-            //            int result = 0;
-            //            result = (value_first - value_second);
-            //            Console.WriteLine("Result: {0}", result);
-            //        }
-            //        if (my_string.Contains("*"))
-            //        {
-            //            int index_ = my_string.IndexOf('*');
-            //            string my_string_one = my_string.Substring(0, index_);
-            //            //   string my_string_heh = my_string.Substring(index_, index_- 1);
-            //            string my_string_two = my_string.Substring(index_+1, (length - 2));
-            //            int value_first = Int32.Parse(my_string_one);
-            //            int value_second = Int32.Parse(my_string_two); 
-            //            int result = 0;
-            //            result = (value_first * value_second);
-            //            Console.WriteLine("Result: {0}", result);
-            //        }
-            //        if (my_string.Contains("/"))
-            //        {
-            //            int index_ = my_string.IndexOf('/');
-            //            string my_string_one = my_string.Substring(0, index_);
-            //            //   string my_string_heh = my_string.Substring(index_, index_- 1);
-            //            string my_string_two = my_string.Substring(index_+1, (length - 2));
-            //            int value_first = Int32.Parse(my_string_one);
-            //            int value_second = Int32.Parse(my_string_two); 
-            //            int result = 0;
-            //            result = (value_first / value_second);
-            //            Console.WriteLine("Result: {0}", result);
-            //        }
-            //    }
-            //    //int result = (value_first + value_second);
-            //    //int result1 = (value_first - value_second);
-            //    //int result2 = (value_first * value_second);
-            //    //int result3 = (value_first / value_second);
-            //    //    int result4 = (value_first + value_second);
-            //  //  Console.WriteLine("Result: {0}", result1);
-            //  //  Console.WriteLine("Result: {0}", result2);
-            //  //  Console.WriteLine("Result: {0}", result3);
-            //  ////  Console.WriteLine("Result: {0}", result);
-            //}
-
             string sign = default;
             while(true)
             {
@@ -94,49 +17,32 @@ namespace First_Application
                 else
                 {
                     Int32 sign_length = sign.Length;
-                    //Console.WriteLine("{0}", sign_length);
                     if (sign.Contains("+"))
                     {
                         int index_ = sign.IndexOf('+');
-                        //Console.WriteLine("{0}", index_);
                         Int32 value_first = Int32.Parse(sign.Substring(0, index_));
-                        //Console.WriteLine("{0}", value_first);
                         Int32 value_second = Int32.Parse(sign.Substring(index_ + 1, (sign_length - index_ - 1)));
-                        //Console.WriteLine("{0}", value_second);
                         Console.WriteLine("Result is following: {0}", (float)(value_first + value_second));
                     }
                     else if (sign.Contains("-"))
                     {
                         Int32 index_ = sign.IndexOf('-');
-                        //Console.WriteLine("{0}", index_);
                         Int32 value_first = Int32.Parse(sign.Substring(0, index_));
-                        //Console.WriteLine("{0}", value_first);
                         Int32 value_second = Int32.Parse(sign.Substring(index_ + 1, (sign_length - index_ - 1)));
-                        //Console.WriteLine("{0}", value_second);
                         Console.WriteLine("Result is following: {0}", (float)(value_first - value_second));
                     }
                     else if (sign.Contains("*"))
                     {
                         Int32 index_ = sign.IndexOf('*');
-                        //Console.WriteLine("{0}", index_);
                         Int32 value_first = Int32.Parse(sign.Substring(0, index_));
-                        //Console.WriteLine("{0}", value_first);
                         Int32 value_second = Int32.Parse(sign.Substring(index_ + 1, (sign_length - index_ - 1)));
-                        //Console.WriteLine("{0}", value_second);
                         Console.WriteLine("Result is following: {0}", (value_first * value_second));
-                       // Console.WriteLine("Result is following: {0}", DoubleConverter.ToExactString(value_first * value_second));
-                       // Console.WriteLine(String.Format("{0:0.00}","Result is following: {0}"), (value_first * value_second));
                     }
                     else if (sign.Contains("/"))
                     {
                         Int32 index_ = sign.IndexOf('/');
-                        //Console.WriteLine("{0}", index_);
                         Int32 value_first = Int32.Parse(sign.Substring(0, index_));
-                        //Console.WriteLine("{0}", value_first);
                         Int32 value_second = Int32.Parse(sign.Substring(index_ + 1, (sign_length - index_ - 1)));
-                        //Console.WriteLine("{0}", value_second);
-                        // Console.WriteLine("Result is following: {0}", (float)(value_first / value_second));
-                        //Console.WriteLine(String.Format("{0:0.00}", "Result is following: {0}"), (float)(value_first / value_second));
                         Console.WriteLine("Result is following: {0:F3}", (((float)value_first) / ((float)value_second)));
                     }
                     else
@@ -147,9 +53,28 @@ namespace First_Application
                 }
                 sign = default;
             }
+
+            Console.WriteLine("Time to show small kind of usage the classes in C#!");
+
+            My_Class Obj = new My_Class("XYZ", "ZYX");
+            Console.WriteLine("{0}", Obj.Name_Property);
+            Console.WriteLine("{0}", Obj.Surname_Property);
+            Obj.Name_Property = "Hello";
+            Obj.Surname_Property = "World";
+            Console.WriteLine("{0}", Obj.Name_Property);
+            Console.WriteLine("{0}", Obj.Surname_Property);
+
+            Console.WriteLine("{0}", Obj.Get_Name());
+            Console.WriteLine("{0}", Obj.Get_Surname());
+
+            Obj.Set_Name("University of");
+            Obj.Set_Surname("Lodz");
+
+            Console.WriteLine("{0} {1}", Obj.Get_Name(), Obj.Get_Surname());
+            Console.WriteLine("\n");
+
             Console.WriteLine("The end of the program! Thanks for using!");
             Console.ReadKey();
-            // Console.WriteLine("Hello World!");
         }
     }
 }
