70d1fbc365045379e50d0279e11f5e54d7a60de2
Damian Andrysiak <andrydamian@gmail.com>
2019-11-22 14:58:13 +0100

    update

--- ADVANCED_PROGRAMMING_II/!PYTHON/Video_Neural_Network/Video_Neural_Network/Video_Neural_Network.py
+++ ADVANCED_PROGRAMMING_II/!PYTHON/Video_Neural_Network/Video_Neural_Network/Video_Neural_Network.py
@@ -5,9 +5,22 @@
 import cv2
 import numpy as np
 import imutils
+import math
+import Functions as fun
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import time
+
+def draw_foo1(my_capture, my_x, my_y):
+    #ax1.plot(xar,yar)
+    #plt.show()
+    cv2.line(my_capture,(my_x,int(800* math.sin(60*1) + my_x)),(my_y,my_x),(255,0,0),5)
+    #cv2.line(my_capture,(my_x,int(800* math.sin(60*1) + my_x)),(my_y,my_x),(255,0,0),5)
+
 
 #cap = cv2.VideoCapture('steel_balls.mp4')
 #cap = cv2.VideoCapture('people_moving.mp4')
+#cap = cv2.VideoCapture('People.mp4')
 cap = cv2.VideoCapture('cars_highway.mp4')
 
 #cap.set(cv2.CAP_PROP_FRAME_WIDTH, 300)
@@ -18,6 +31,10 @@ cap = cv2.VideoCapture('cars_highway.mp4')
 
 ret, frame1 = cap.read()
 ret, frame2 = cap.read()
+
+xar = []
+yar = []
+
 
 while cap.isOpened():
     #ret, frame = cap.read()
@@ -33,8 +50,13 @@ while cap.isOpened():
         (x,y,w,h) = cv2.boundingRect(contour)
         if cv2.contourArea(contour) < 2000 : #if area of the countour, dont draw anything
             continue
-        cv2.rectangle(frame1,(x,y), (x+w,y+h),(0,255,0), 2)
-        cv2.putText(frame1, "Status: {}".format("Movement"), (10,20), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,0,255),3)
+        #xar.append(int(x))
+        #yar.append(int(y))
+    draw_foo1(frame1,x,y)
+        #draw_foo1(frame1,y,x)
+        #cv2.rectangle(frame1,(x,y), (x+w,y+h),(0,255,0), 2)
+
+    cv2.putText(frame1, "Status: {}".format("Movement"), (10,20), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,0,255),3)
     #cv2.drawContours(frame1,contours, -1, (0,255,0), 2)
     cv2.imshow("Cars",frame1)
 
@@ -45,8 +67,8 @@ while cap.isOpened():
     #cv2.imshow("Inter",frame)
 
 
-    frame1 = imutils.resize(frame1, width=1920, height = 1080)
-    frame2 = imutils.resize(frame2, width=1920, height = 1080)
+    frame1 = imutils.resize(frame1, width=800, height = 800)
+    frame2 = imutils.resize(frame2, width=800, height = 800)
 
     if cv2.waitKey(40) == 27:   #press ESC
         break
