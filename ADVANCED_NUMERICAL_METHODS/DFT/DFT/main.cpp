2c7f12c25362ab9e3dfb41e23d68bc5798da59b5
Damian Andrysiak <andrydamian@gmail.com>
2020-01-08 18:03:40 +0100

    fft cooley tukey algorithm

--- ADVANCED_NUMERICAL_METHODS/DFT/DFT/main.cpp
+++ ADVANCED_NUMERICAL_METHODS/DFT/DFT/main.cpp
@@ -9,9 +9,43 @@
 #include <vector>
 
 #define NEW_LINE '\n'
+const double PI = acos(-1);
 
 void fft(int *x_in,  std::complex<double> *x_out, int N);
 void fft_rec(std::complex<double> *x, int N);
+
+void fft2(std::vector<std::complex<double>>& a, bool invert)
+{ 	
+	int n = a.size(); 
+	if (n == 1) 	
+		return; 	
+
+	std::vector<std::complex<double>> a0(n / 2), a1(n / 2);
+
+	for (int i = 0; 2 * i < n; i++)
+	{ 		
+		a0[i] = a[2 * i]; 	
+		a1[i] = a[2 * i + 1]; 
+	} 	
+
+	fft2(a0, invert);
+	fft2(a1, invert);
+
+	double ang = 2 * M_PI / n * (invert ? -1 : 1); 
+	std::complex<double> w(1), wn(cos(ang), sin(ang));
+
+	for (int i = 0; 2 * i < n; i++)
+	{ 		
+		a[i] = a0[i] + w * a1[i]; 
+		a[i + n / 2] = a0[i] - w * a1[i]; 	
+		if (invert) 
+		{ 			
+			a[i] /= 2; 		
+			a[i + n / 2] /= 2; 		
+		} 		
+		w *= wn; 	
+	}
+} 
 
 int main(int argc, char* argv[])
 {
@@ -31,6 +65,7 @@ int main(int argc, char* argv[])
 	file_in >> N;
 
 	_STD vector<std::complex<double>> data(N);
+	_STD vector<std::complex<double>> data2(N);
 	//_STD vector<double> Cn(N);
 	std::complex<double>* Cn = new std::complex<double>[N];
 
@@ -45,20 +80,35 @@ int main(int argc, char* argv[])
 		Cn[i]._Val[1] = 0.0;
 	}
 
-	//_STD cout << "i: " << i << " Real: " << temp_Re << " Imaginary: " << temp_Im << NEW_LINE;
-	for (size_t i = 0; i < N; ++i)
-	{
-		__int32 temp = data[i]._Val[0];
-		fft(&temp, Cn, N);
-		temp = data[i]._Val[1];
-		fft(&temp, Cn, N);
-	}
+	data2 = data;
 
-	for (size_t i = 0; i < N; ++i)
-	{
-		_STD cout << Cn[i]._Val[0] << " " << Cn[i]._Val[1] << NEW_LINE;
-	}
-	_STD cout << NEW_LINE;
+	//_STD cout << "i: " << i << " Real: " << temp_Re << " Imaginary: " << temp_Im << NEW_LINE;
+	//for (size_t i = 0; i < N; ++i)
+	//{
+	//	__int32 temp = data[i]._Val[0];
+	//	fft(&temp, Cn, N);
+	//	temp = data[i]._Val[1];
+	//	fft(&temp, Cn, N);
+	//}
+
+	fft2(data2, false);
+
+	//for (size_t i = 0; i < N; ++i)
+	//{
+	//	//_STD cout << Cn[i]._Val[0] << " " << Cn[i]._Val[1] << NEW_LINE;
+	//	_STD cout << data2[i]._Val[0] << " " << data2[i]._Val[1] << NEW_LINE;
+	//}
+	//_STD cout << NEW_LINE;
+
+	for (auto& x : data2)
+		printf("( %4.4f , %4.4f )\n", x.real() > 0.0001 ? x.real() : 0., x.imag() > 0.0001 ? x.imag() : 0.);
+	printf("\n\n\n");
+
+	fft2(data2, true);
+
+	for (auto& x : data2)
+		printf("( %4.4f , %4.4f )\n", x.real() > 0.0001 ? x.real() : 0., x.imag() > 0.0001 ? x.imag() : 0.);
+	printf("\n\n\n");
 
 	file_in.close();
 	system("pause");
@@ -105,3 +155,122 @@ void fft_rec(std::complex<double>* x, int N)
 		delete[] even;
 	}
 }
+
+
+//#define _USE_MATH_DEFINES
+//#include <iostream> 
+//#include <complex>
+//#include <cmath>
+//#include <vector>
+//#include <fstream>
+//
+//using cd = std::complex<double>; 
+//
+//const double PI = acos(-1); 
+//
+//void fft(std::vector<cd>& a, bool invert)
+//{ 	
+//	int n = a.size(); 
+//	if (n == 1) 	
+//		return; 	
+//
+//	std::vector<cd> a0(n / 2), a1(n / 2); 
+//
+//	for (int i = 0; 2 * i < n; i++)
+//	{ 		
+//		a0[i] = a[2 * i]; 	
+//		a1[i] = a[2 * i + 1]; 
+//	} 	
+//	fft(a0, invert); 	
+//	fft(a1, invert); 	
+//	double ang = 2 * M_PI / n * (invert ? -1 : 1); 
+//	cd w(1), wn(cos(ang), sin(ang));
+//
+//	for (int i = 0; 2 * i < n; i++)
+//	{ 		
+//		a[i] = a0[i] + w * a1[i]; 
+//		a[i + n / 2] = a0[i] - w * a1[i]; 	
+//		if (invert) 
+//		{ 			
+//			a[i] /= 2; 		
+//			a[i + n / 2] /= 2; 		
+//		} 		
+//		w *= wn; 	
+//	}
+//} 
+//
+//
+//int main()
+//{ 	
+//	std::fstream in; 
+//	in.open("file.in", std::ios::in); 
+//	size_t N; 
+//	double temp; 	
+//	in >> N; 
+//	std::vector<std::complex<double>> data(N);
+//	std::vector<std::complex<double>> data2(N); 
+//	std::vector<std::complex<double>> data3; 	
+//	//std::vector<double> Cn(N); 	
+//	for (size_t i = 0; i < N; ++i) 
+//	{ 		
+//		in >> temp; 	
+//		data[i].real(temp); 	
+//		in >> temp; 	
+//		data[i].imag(temp); 
+//	} 
+//	data2 = data; 
+//
+//	fft(data2, false); 
+//
+//	for (auto& x : data2) 
+//		printf("( %4.4f , %4.4f )\n", x.real() > 0.0001? x.real():0.,x.imag() > 0.0001 ? x.imag() : 0.); 
+//	printf("\n\n\n"); 
+//
+///*	fft(data2, true); 
+//
+//	for (auto& x : data2) 
+//		printf("( %4.4f , %4.4f )\n", x.real() > 0.0001? x.real():0.,x.imag() > 0.0001 ? x.imag() : 0.); 
+//	printf("\n\n\n"); */	
+//
+//	for (size_t n = 0; n < N; ++n) 
+//	{ 		
+//		std::complex<double> num(0., 0.); 
+//		// od 0 do n/2 	
+//		if (n < N / 2.) 	
+//		{ 			
+//			for (int m = 0; m < N / 2; ++m) 	
+//			{ 			
+//				num += (data[2 * m] + (data[2 * m + 1] * std::exp(std::complex<double>(0., 1.) * ((2. * M_PI * n) / (double)N)))) * std::exp(std::complex<double>(0., 1.) * (2 * M_PI * m * n) / (N / 2.));
+//			} 		
+//		} 	
+//		else 		
+//		{ 			
+//			for (int m = 0; m < N / 2; ++m) 
+//			{ 				
+//				num += (data[2 * m] - (data[2 * m + 1] * std::exp(std::complex<double>(0., 1.) * ((2. * M_PI * n) / (double)N)))) * std::exp(std::complex<double>(0., 1.) * (2 * M_PI * m *(n + (N / 2.))) / (N / 2.)); 	
+//			} 
+//		} 	
+//
+//		printf("( %4.4f , %4.4f )\n", num.real() > 0.0001 ? num.real() : 0., num.imag() > 0.0001 ? num.imag() : 0.); 
+//		data3.push_back(num); 	
+//		if (n == N / 2 -1 )
+//			printf("\n"); 	
+//		//std::cout << num << '\n'; 	
+//	} 	
+//	printf("\n\n\n"); 
+//
+//	//for (size_t n = 0; n < N; ++n) 
+//	//{ 	
+//	//	std::complex<double> num(0., 0.); 
+//	//	for (int k = 0; k < N; ++k) 	
+//	//	{ 	
+//	//		num += data3[k] * std::exp(std::complex<double>(0., 1.) * ((2. * M_PI * n * k) / (double)N)); 
+//	//	} 
+//	//	printf("( %4.4f , %4.4f )\n", num.real() > 0.0001 ? num.real() : 0., num.imag() > 0.0001 ? num.imag() : 0.); 
+//	//	
+//	//std::cout << num << '\n'; 	
+//	//} 
+//
+//	_STD cin.get();
+//	return 0;
+//}
