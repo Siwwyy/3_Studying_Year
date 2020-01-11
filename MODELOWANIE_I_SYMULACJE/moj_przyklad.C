c314d79b1740aabca58b992b055ded231a6c5645
Damian Andrysiak <andrydamian@gmail.com>
2020-01-11 21:23:06 +0100

    update along Main Project at Simulations

--- MODELOWANIE_I_SYMULACJE/moj_przyklad.C
+++ MODELOWANIE_I_SYMULACJE/moj_przyklad.C
@@ -5,36 +5,45 @@
 void moj_przyklad()
 {
 	using namespace std;
-  Int_t n=99,i,j ;
+  Int_t n=3000;
   Float_t x1[3000];
   std::ifstream file;
 
    file.open("E:\\!!Projects VS\\!!!3_Studying_Year\\3_Studying_Year\\MODELOWANIE_I_SYMULACJE\\Main_Project\\Main_Project\\file.out");
-   for(i=0;i<3000;i=i+1)
+   for(int i=0;i<3000;i=i+1)
      {
 	  file>>x1[i];
      }
   file.close();
 
-   TH1F *im1 = new TH1F("dane","sredni czas przejazdu trasa WZ",60, 0, 60);
-    for(i=0;i<3000;i=i+1)
-	{ 
-		im1->Fill(x1[i]);
-	}
+TH1F *h = new TH1F("h","sredni czas przejazdu trasa WZ",60, 0, 60);
+   h->SetMaximum(180);
 
-TCanvas *canvas = new TCanvas("im1");
+   for (int i=0;i<3000;i++)
+   {
+      h->Fill(x1[i]);
+   }
 
-canvas->SetFillColor(0);
-canvas->Divide(1,1); 
-
-
- canvas->cd(1);
- im1->SetFillColor(2);
- im1->SetFillStyle(3002);
- im1->Draw();
-
-
-TF1 *ff = new TF1("ff","[0]+[1]*x-[2]*pow(x,2)",40.,80.);
-  im1->Fit(ff);
- 
+   Double_t par[9];
+   TF1 *g1    = new TF1("g1","gaus",10,25);
+   TF1 *g2    = new TF1("g2","gaus",30,40);
+   TF1 *g3    = new TF1("g3","gaus",45,60);
+   TF1 *total = new TF1("total","gaus(0)+gaus(3)+gaus(6)",0,60);
+   total->SetLineColor(2);
+   h->Fit(g1,"R");
+   h->Fit(g2,"R+");
+   h->Fit(g3,"R+");
+   g1->GetParameters(&par[0]);
+   g2->GetParameters(&par[3]);
+   g3->GetParameters(&par[6]);
+   total->SetParameters(par);
+   
+   TCanvas *canvas = new TCanvas("h");
+   canvas->SetFillColor(0);
+   canvas->Divide(1,1);
+   canvas->cd(1);
+   h->SetFillColor(2);
+   h->SetFillStyle(3002);
+   
+   h->Fit(total,"R+"); 
 }
