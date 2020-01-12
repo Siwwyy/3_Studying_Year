14eac77170b34e4d6d1b3b21c715b20629b6219c
Damian Andrysiak <andrydamian@gmail.com>
2020-01-12 22:36:08 +0100

    up

--- ASP_NET/!MY_PAGE/!MY_PAGE/Controllers/HomeController.cs
+++ ASP_NET/!MY_PAGE/!MY_PAGE/Controllers/HomeController.cs
@@ -58,27 +58,33 @@ namespace _MY_PAGE.Controllers
                 db.Mark.Add(Ha);
 
 
-
+                db.SaveChanges();
                 Informacje_w_ogloszeniu Ha2 = new Informacje_w_ogloszeniu();
                 Ha2.ID = rand.Next(00000, 999999);
                 Ha2.Moc_Silnika = int.Parse(silnik);
                 Ha2.Przebieg_do = int.Parse(przebieg);
                 Ha2.Cena = int.Parse(cena);
                 db.Info.Add(Ha2);
-
+                db.SaveChanges();
                 Kraj_Pochodzenia Ha3 = new Kraj_Pochodzenia();
                 Ha3.ID = rand.Next(00000, 999999);
                 Ha3.Nazwa_Kraj_Pochodzenia = pochodzenie;
                 db.Country.Add(Ha3);
 
-
+                db.SaveChanges();
                 Sprzedawca Ha4 = new Sprzedawca();
                 Ha4.ID = rand.Next(00000, 999999);
                 Ha4.Imie = imie;
-                Ha4.Nazwiska = imie;
+                Ha4.Nazwiska = nazwisko;
+                Ha4.Pesel = rand.Next(00000, 999999);
                 db.Seller.Add(Ha4);
+                db.SaveChanges();
+                Rodzaj_Paliwa Ha5 = new Rodzaj_Paliwa();
+                Ha5.ID = rand.Next(00000, 999999);
+                Ha5.Nazwa_Rodzaj = fuel;
+                db.Fuel_Type.Add(Ha5);
 
-
+                db.SaveChanges();
 
                 Model_Pojazdu Ha1 = new Model_Pojazdu();
                 Ha1.ID = rand.Next(00000, 999999);
@@ -86,8 +92,15 @@ namespace _MY_PAGE.Controllers
                 Ha1.Marka_ID = Ha.ID;
                 Ha1.Informations = Ha2;
                 Ha1.Kraj_Pochodzenia_ID = Ha3.ID;
+                Ha1.Rodzaj_Paliwa_ID = Ha5.ID;
+                Ha1.Country_Of_Origin = Ha3;
+                Ha1.Fuel_Type = Ha5;
+                Ha1.Country_Of_Origin = Ha3;
+                Ha1.Marka = Ha;
+                Ha1.Seller = Ha4;
                 Ha1.Sprzedawca_ID = Ha4.ID;
                 db.Model.Add(Ha1);
+                db.SaveChanges();
             }
             return View();
         }
