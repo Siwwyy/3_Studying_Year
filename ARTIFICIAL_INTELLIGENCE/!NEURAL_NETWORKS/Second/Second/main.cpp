2adf43fbf70e4b33a944b24c5868523bc92d2916
Damian Andrysiak <andrydamian@gmail.com>
2020-01-13 18:33:22 +0100

    update

--- ARTIFICIAL_INTELLIGENCE/!NEURAL_NETWORKS/Second/Second/main.cpp
+++ ARTIFICIAL_INTELLIGENCE/!NEURAL_NETWORKS/Second/Second/main.cpp
@@ -12,11 +12,70 @@
 #define NEW_LINE '\n'
 #define e 2.71828182
 
-const float f1(const float value);
-const float f2(const float value);
-const float f3(const float value);
+const float f1(const float value);	//progowa
+const float f2(const float value);	//liniowa	
+const float f3(const float value);	//sigmoidalna
 const float normalized_point(const float value, const __int16 min, const __int16 max, const __int16 new_min, const __int16 new_max);
 void initial_weights(float * weights, const size_t size);
+
+class Pixel
+{
+	__int16 R;
+	__int16 G;
+	__int16 B;
+public:
+	Pixel():
+		R(NULL),
+		G(NULL),
+		B(NULL)
+	{
+
+	}
+	Pixel(const __int16 R, const __int16 G, const __int16 B) :
+		R(R),
+		G(G),
+		B(B)
+	{
+
+	}
+
+	void Set_R(const __int16 R)
+	{
+		this->R = R;
+	}
+	void Set_G(const __int16 G)
+	{
+		this->G = G;
+	}
+
+	void Set_B(const __int16 B)
+	{
+		this->B = B;
+	}
+
+	const __int16 Get_R() const
+	{
+		return this->R;
+	}
+	const __int16 Get_G() const
+	{
+		return this->G;
+	}
+	const __int16 Get_B() const
+	{
+		return this->B;
+	}
+
+	~Pixel()
+	{
+		this->R = 0;
+		this->G = 0;
+		this->B = 0;
+	}
+};
+
+void Save_To_PPM(const _STD vector<_STD vector<Pixel>>& vec, const char * file_name);
+
 
 int main(int argc, char* argv[])
 {
@@ -25,6 +84,13 @@ int main(int argc, char* argv[])
 	using _STD cin;
 
 	__int16 choice{};
+
+	_STD vector<_STD vector<Pixel>> Matrix(500);
+
+	for (typename _STD vector<_STD vector<Pixel>>::iterator vec_iterator = Matrix.begin(); vec_iterator != Matrix.end(); ++vec_iterator)
+	{
+		vec_iterator->resize(500);
+	}
 
 	_STD cin >> choice;
 	if (choice == 0)	//without bias
@@ -50,18 +116,22 @@ int main(int argc, char* argv[])
 						COORD c;
 						c.X = i;
 						c.Y = j;
+
 						if (y == 1)
 						{
-							SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-							_STD cout << 'o';
+							Matrix[i][j].Set_R(255);
+							Matrix[i][j].Set_G(0);
+							Matrix[i][j].Set_B(0);
 						}
 						else
 						{
-							SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-							_STD cout << 'x';
+							Matrix[i][j].Set_R(0);
+							Matrix[i][j].Set_G(0);
+							Matrix[i][j].Set_B(255);
 						}
 					}
 				}
+				Save_To_PPM(Matrix, "progowaI_bezbiasu.ppm");
 			}
 			break;
 			case 2:
@@ -77,18 +147,34 @@ int main(int argc, char* argv[])
 						COORD c;
 						c.X = i;
 						c.Y = j;
-						if (y <= 1 && y > 0.5)
+
+						if (y < -2)
 						{
-							SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-							_STD cout << 'o';
+							Matrix[i][j].Set_R(8);
+							Matrix[i][j].Set_G(37);
+							Matrix[i][j].Set_B(103);
 						}
-						else
+						else if(y >= -2 && y < 0)
 						{
-							SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-							_STD cout << 'x';
+							Matrix[i][j].Set_R(0);
+							Matrix[i][j].Set_G(0);
+							Matrix[i][j].Set_B(255);
+						}
+						else if (y >= 0 && y <= 2)
+						{
+							Matrix[i][j].Set_R(0);
+							Matrix[i][j].Set_G(255);
+							Matrix[i][j].Set_B(0);
+						}
+						else if (y >= -2 && y <= 0)
+						{
+							Matrix[i][j].Set_R(255);
+							Matrix[i][j].Set_G(0);
+							Matrix[i][j].Set_B(0);
 						}
 					}
 				}
+				Save_To_PPM(Matrix, "liniowaI_bezbiasu.ppm");
 			}
 			break;
 			case 3:
@@ -104,18 +190,34 @@ int main(int argc, char* argv[])
 						COORD c;
 						c.X = i;
 						c.Y = j;
-						if (y <= 1 && y > 0.5)
+
+						if (y >= 0 && y < 0.25)
 						{
-							SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-							_STD cout << 'o';
+							Matrix[i][j].Set_R(255);
+							Matrix[i][j].Set_G(255);
+							Matrix[i][j].Set_B(0);
 						}
-						else
+						else if (y >= 0.25 && y < 0.5)
 						{
-							SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-							_STD cout << 'x';
+							Matrix[i][j].Set_R(0);
+							Matrix[i][j].Set_G(0);
+							Matrix[i][j].Set_B(255);
+						}
+						else if (y >= 0.5 && y < 0.75)
+						{
+							Matrix[i][j].Set_R(0);
+							Matrix[i][j].Set_G(255);
+							Matrix[i][j].Set_B(0);
+						}
+						else if (y >= 0.75 && y <= 1)
+						{
+							Matrix[i][j].Set_R(255);
+							Matrix[i][j].Set_G(0);
+							Matrix[i][j].Set_B(0);
 						}
 					}
 				}
+				Save_To_PPM(Matrix, "sigmoidalnaI_bezbiasu.ppm");
 			}
 			break;
 			//second neural
@@ -142,18 +244,22 @@ int main(int argc, char* argv[])
 						COORD c;
 						c.X = i;
 						c.Y = j;
+
 						if (y == 1)
 						{
-							SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-							_STD cout << 'o';
+							Matrix[i][j].Set_R(255);
+							Matrix[i][j].Set_G(0);
+							Matrix[i][j].Set_B(0);
 						}
 						else
 						{
-							SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-							_STD cout << 'x';
+							Matrix[i][j].Set_R(0);
+							Matrix[i][j].Set_G(0);
+							Matrix[i][j].Set_B(255);
 						}
 					}
 				}
+				Save_To_PPM(Matrix, "progowaII_bezbiasu.ppm");
 			}
 			break;
 			case 5:
@@ -179,18 +285,34 @@ int main(int argc, char* argv[])
 						COORD c;
 						c.X = i;
 						c.Y = j;
-						if (y <= 1 && y > 0.5)
+
+						if (y < -2)
 						{
-							SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-							_STD cout << 'o';
+							Matrix[i][j].Set_R(8);
+							Matrix[i][j].Set_G(37);
+							Matrix[i][j].Set_B(103);
 						}
-						else
+						else if (y >= -2 && y < 0)
 						{
-							SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-							_STD cout << 'x';
+							Matrix[i][j].Set_R(0);
+							Matrix[i][j].Set_G(0);
+							Matrix[i][j].Set_B(255);
+						}
+						else if (y >= 0 && y <= 2)
+						{
+							Matrix[i][j].Set_R(0);
+							Matrix[i][j].Set_G(255);
+							Matrix[i][j].Set_B(0);
+						}
+						else if (y >= -2 && y <= 0)
+						{
+							Matrix[i][j].Set_R(255);
+							Matrix[i][j].Set_G(0);
+							Matrix[i][j].Set_B(0);
 						}
 					}
 				}
+				Save_To_PPM(Matrix, "liniowaII_bezbiasu.ppm");
 			}
 			break;
 			case 6:
@@ -216,18 +338,34 @@ int main(int argc, char* argv[])
 						COORD c;
 						c.X = i;
 						c.Y = j;
-						if (y <= 1 && y > 0.5)
+
+						if (y >= 0 && y < 0.25)
 						{
-							SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-							_STD cout << 'o';
+							Matrix[i][j].Set_R(255);
+							Matrix[i][j].Set_G(255);
+							Matrix[i][j].Set_B(0);
 						}
-						else
+						else if (y >= 0.25 && y < 0.5)
 						{
-							SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-							_STD cout << 'x';
+							Matrix[i][j].Set_R(0);
+							Matrix[i][j].Set_G(0);
+							Matrix[i][j].Set_B(255);
+						}
+						else if (y >= 0.5 && y < 0.75)
+						{
+							Matrix[i][j].Set_R(0);
+							Matrix[i][j].Set_G(255);
+							Matrix[i][j].Set_B(0);
+						}
+						else if (y >= 0.75 && y <= 1)
+						{
+							Matrix[i][j].Set_R(255);
+							Matrix[i][j].Set_G(0);
+							Matrix[i][j].Set_B(0);
 						}
 					}
 				}
+				Save_To_PPM(Matrix, "sigmoidalnaI_bezbiasu.ppm");
 			}
 			break;
 
@@ -259,18 +397,22 @@ int main(int argc, char* argv[])
 						COORD c;
 						c.X = i;
 						c.Y = j;
+
 						if (y == 1)
 						{
-							SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-							_STD cout << 'o';
+							Matrix[i][j].Set_R(255);
+							Matrix[i][j].Set_G(0);
+							Matrix[i][j].Set_B(0);
 						}
 						else
 						{
-							SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-							_STD cout << 'x';
+							Matrix[i][j].Set_R(0);
+							Matrix[i][j].Set_G(0);
+							Matrix[i][j].Set_B(255);
 						}
 					}
 				}
+				Save_To_PPM(Matrix, "progowaI_zbiasem.ppm");
 			}
 			break;
 			case 2:
@@ -287,18 +429,34 @@ int main(int argc, char* argv[])
 						COORD c;
 						c.X = i;
 						c.Y = j;
-						if (y <= 1 && y > 0.5)
+
+						if (y < -2)
 						{
-							SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-							_STD cout << 'o';
+							Matrix[i][j].Set_R(8);
+							Matrix[i][j].Set_G(37);
+							Matrix[i][j].Set_B(103);
 						}
-						else
+						else if (y >= -2 && y < 0)
 						{
-							SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-							_STD cout << 'x';
+							Matrix[i][j].Set_R(0);
+							Matrix[i][j].Set_G(0);
+							Matrix[i][j].Set_B(255);
+						}
+						else if (y >= 0 && y <= 2)
+						{
+							Matrix[i][j].Set_R(0);
+							Matrix[i][j].Set_G(255);
+							Matrix[i][j].Set_B(0);
+						}
+						else if (y >= -2 && y <= 0)
+						{
+							Matrix[i][j].Set_R(255);
+							Matrix[i][j].Set_G(0);
+							Matrix[i][j].Set_B(0);
 						}
 					}
 				}
+				Save_To_PPM(Matrix, "liniowaI_zbiasem.ppm");
 			}
 			break;
 			case 3:
@@ -315,18 +473,34 @@ int main(int argc, char* argv[])
 						COORD c;
 						c.X = i;
 						c.Y = j;
-						if (y <= 1 && y > 0.5)
+
+						if (y >= 0 && y < 0.25)
 						{
-							SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-							_STD cout << 'o';
+							Matrix[i][j].Set_R(255);
+							Matrix[i][j].Set_G(255);
+							Matrix[i][j].Set_B(0);
 						}
-						else
+						else if (y >= 0.25 && y < 0.5)
 						{
-							SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-							_STD cout << 'x';
+							Matrix[i][j].Set_R(0);
+							Matrix[i][j].Set_G(0);
+							Matrix[i][j].Set_B(255);
+						}
+						else if (y >= 0.5 && y < 0.75)
+						{
+							Matrix[i][j].Set_R(0);
+							Matrix[i][j].Set_G(255);
+							Matrix[i][j].Set_B(0);
+						}
+						else if (y >= 0.75 && y <= 1)
+						{
+							Matrix[i][j].Set_R(255);
+							Matrix[i][j].Set_G(0);
+							Matrix[i][j].Set_B(0);
 						}
 					}
 				}
+				Save_To_PPM(Matrix, "sigmoidalnaI_zbiasem.ppm");
 			}
 			break;
 			//second neural
@@ -359,18 +533,22 @@ int main(int argc, char* argv[])
 						COORD c;
 						c.X = i;
 						c.Y = j;
+
 						if (y == 1)
 						{
-							SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-							_STD cout << 'o';
+							Matrix[i][j].Set_R(255);
+							Matrix[i][j].Set_G(0);
+							Matrix[i][j].Set_B(0);
 						}
 						else
 						{
-							SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-							_STD cout << 'x';
+							Matrix[i][j].Set_R(0);
+							Matrix[i][j].Set_G(0);
+							Matrix[i][j].Set_B(255);
 						}
 					}
 				}
+				Save_To_PPM(Matrix, "progowaII_zbiasem.ppm");
 			}
 			break;
 			case 5:
@@ -401,18 +579,34 @@ int main(int argc, char* argv[])
 						COORD c;
 						c.X = i;
 						c.Y = j;
-						if (y <= 1 && y > 0.5)
+
+						if (y < -2)
 						{
-							SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-							_STD cout << 'o';
+							Matrix[i][j].Set_R(8);
+							Matrix[i][j].Set_G(37);
+							Matrix[i][j].Set_B(103);
 						}
-						else
+						else if (y >= -2 && y < 0)
 						{
-							SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-							_STD cout << 'x';
+							Matrix[i][j].Set_R(0);
+							Matrix[i][j].Set_G(0);
+							Matrix[i][j].Set_B(255);
+						}
+						else if (y >= 0 && y <= 2)
+						{
+							Matrix[i][j].Set_R(0);
+							Matrix[i][j].Set_G(255);
+							Matrix[i][j].Set_B(0);
+						}
+						else if (y >= -2 && y <= 0)
+						{
+							Matrix[i][j].Set_R(255);
+							Matrix[i][j].Set_G(0);
+							Matrix[i][j].Set_B(0);
 						}
 					}
 				}
+				Save_To_PPM(Matrix, "liniowaII_zbiasem.ppm");
 			}
 			break;
 			case 6:
@@ -443,18 +637,34 @@ int main(int argc, char* argv[])
 						COORD c;
 						c.X = i;
 						c.Y = j;
-						if (y <= 1 && y > 0.5)
+
+						if (y >= 0 && y < 0.25)
 						{
-							SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-							_STD cout << 'o';
+							Matrix[i][j].Set_R(255);
+							Matrix[i][j].Set_G(255);
+							Matrix[i][j].Set_B(0);
 						}
-						else
+						else if (y >= 0.25 && y < 0.5)
 						{
-							SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-							_STD cout << 'x';
+							Matrix[i][j].Set_R(0);
+							Matrix[i][j].Set_G(0);
+							Matrix[i][j].Set_B(255);
+						}
+						else if (y >= 0.5 && y < 0.75)
+						{
+							Matrix[i][j].Set_R(0);
+							Matrix[i][j].Set_G(255);
+							Matrix[i][j].Set_B(0);
+						}
+						else if (y >= 0.75 && y <= 1)
+						{
+							Matrix[i][j].Set_R(255);
+							Matrix[i][j].Set_G(0);
+							Matrix[i][j].Set_B(0);
 						}
 					}
 				}
+				Save_To_PPM(Matrix, "sigmoidalnaII_zbiasem.ppm");
 			}
 			break;
 
@@ -470,6 +680,7 @@ int main(int argc, char* argv[])
 
 
 const float f1(const float value)
+//progowa
 {
 	if (value >= 0)
 	{
@@ -482,11 +693,13 @@ const float f1(const float value)
 }
 
 const float f2(const float value)
+//liniowa
 {
 	return value;
 }
 
 const float f3(const float value)
+//sigmoidalna
 {
 	return static_cast<float>(1.0f / (1.0f + static_cast<float>(pow((1.0f / e), value))));
 }
@@ -507,4 +720,44 @@ void initial_weights(float* weights, const size_t size)
 	{
 		weights[i] = dis(generator);
 	}
+}
+
+void Save_To_PPM(const::std::vector<::std::vector<Pixel>>& vec, const char * file_name)
+{
+	std::fstream file_out;
+	file_out.open(file_name, _STD ios_base::out);
+	if (file_out.good() == false)
+	{
+		exit(0);
+	}
+	else
+	{
+		file_out << "P3";
+		file_out << '\n';
+		file_out << "# Created by Damian Andrysiak";
+		file_out << '\n';
+		file_out << vec.size();
+		file_out << ' ';
+		file_out << vec[0].size();
+		file_out << '\n';
+		file_out << 255;
+		file_out << '\n';
+
+		for (auto iter = vec.begin(); iter != vec.end(); ++iter)
+		{
+			for (auto iter_second = iter->begin(); iter_second != iter->end(); ++iter_second)
+			{
+				file_out << iter_second->Get_R();
+				file_out << ' ';
+				file_out << iter_second->Get_G();
+				file_out << ' ';
+				file_out << iter_second->Get_B();
+				file_out << ' ';
+			}
+
+			file_out << NEW_LINE;
+		}
+	}
+
+	file_out.close();
 }
