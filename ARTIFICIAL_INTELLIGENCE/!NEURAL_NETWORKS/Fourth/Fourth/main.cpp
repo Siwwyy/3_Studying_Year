9ffd242aa631cc8908a5c274fa40044164e85db5
Damian Andrysiak <andrydamian@gmail.com>
2020-01-18 20:04:05 +0100

    up

--- ARTIFICIAL_INTELLIGENCE/!NEURAL_NETWORKS/Fourth/Fourth/main.cpp
+++ ARTIFICIAL_INTELLIGENCE/!NEURAL_NETWORKS/Fourth/Fourth/main.cpp
@@ -7,6 +7,7 @@
 #include <random>
 #include <iomanip>
 #include <math.h>
+#include <cstdint>
 #include <windows.h>
 
 #define NEW_LINE '\n'
@@ -18,54 +19,54 @@ const void Display_Matrix(const __int16 const* const* Matrix, const size_t & wid
 
 int main(int argc, char* argv[])
 {
-	//using _STD cout;
-	//using _STD endl;
-	//using _STD cin;
-
-	//__int16 ** Matrix{};
-	//size_t width{35};
-	//size_t height{4};
-	//Show_Memory(Matrix, width, height);
-	//Matrix = Read_Inputs("file.in", width, height);
-	//Display_Matrix(Matrix, width, height);
-	//Show_Memory(Matrix, width, height);
-	////DEALLOCATE ALLOCATED MEMORY
-	//for (size_t i = 0; i < height; ++i)
-	//{
-	//	delete[] Matrix[i];
-	//	//Matrix[i] = nullptr;
-	//}
-	//delete[] Matrix;
-	////Matrix = nullptr;
-	//Show_Memory(Matrix, width, height);
-	//system("pause");
-	//return 0;
-
-
 	using _STD cout;
 	using _STD endl;
 	using _STD cin;
 
-	__int16** Matrix{};
-	size_t width{ 35 };
-	size_t height{ 4 };
-	Matrix = new __int16* [height];
-	for (size_t i = 0; i < height; ++i)
-	{
-		Matrix[i] = new __int16[width];
-		for (size_t j = 0; j < width; ++j)
-		{
-			Matrix[i][j] = NULL;
-		}
-	};
+	__int16 ** Matrix{};
+	size_t width{35};
+	size_t height{4};
+	Show_Memory(Matrix, width, height);
+	Matrix = Read_Inputs("file.in", width, height);
+	Display_Matrix(Matrix, width, height);
+	Show_Memory(Matrix, width, height);
 	//DEALLOCATE ALLOCATED MEMORY
 	for (size_t i = 0; i < height; ++i)
 	{
 		delete[] Matrix[i];
+		//Matrix[i] = nullptr;
 	}
 	delete[] Matrix;
+	//Matrix = nullptr;
+	Show_Memory(Matrix, width, height);
 	system("pause");
 	return 0;
+
+
+	//using _STD cout;
+	//using _STD endl;
+	//using _STD cin;
+
+	//__int16** Matrix{};
+	//size_t width{ 35 };
+	//size_t height{ 4 };
+	//Matrix = new __int16* [height];
+	//for (size_t i = 0; i < height; ++i)
+	//{
+	//	Matrix[i] = new __int16[width];
+	//	for (size_t j = 0; j < width; ++j)
+	//	{
+	//		Matrix[i][j] = NULL;
+	//	}
+	//};
+	////DEALLOCATE ALLOCATED MEMORY
+	//for (size_t i = 0; i < height; ++i)
+	//{
+	//	delete[] Matrix[i];
+	//}
+	//delete[] Matrix;
+	//system("pause");
+	//return 0;
 }
 
 __int16** Read_Inputs(const char* file_name, size_t& width, size_t& height)
