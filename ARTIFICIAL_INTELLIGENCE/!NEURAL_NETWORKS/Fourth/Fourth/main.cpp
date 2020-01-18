62fbf57e091ecee28082fcc7d2da4aa952991e22
Damian Andrysiak <andrydamian@gmail.com>
2020-01-18 23:48:42 +0100

    up

--- ARTIFICIAL_INTELLIGENCE/!NEURAL_NETWORKS/Fourth/Fourth/main.cpp
+++ ARTIFICIAL_INTELLIGENCE/!NEURAL_NETWORKS/Fourth/Fourth/main.cpp
@@ -13,9 +13,11 @@
 #define NEW_LINE '\n'
 
 //const __int16 const* const* Read_Inputs(const char * file_name, size_t & width, size_t & height);
-__int16 ** Read_Inputs(const char * file_name, size_t & width, size_t & height);
+__int16** Read_Inputs(const char* file_name, size_t& width, size_t& height);
 const void Show_Memory(const __int16 const* const* Matrix, const size_t& width, const size_t& height);
-const void Display_Matrix(const __int16 const* const* Matrix, const size_t & width, const size_t & height);
+const void Display_Matrix(const __int16 const* const* Matrix, const size_t& width, const size_t& height);
+const void Display_Single_Matrix(const __int16 const* Matrix, const size_t& width);
+const void Hopfield(__int16** Matrix, const size_t& width);
 
 int main(int argc, char* argv[])
 {
@@ -23,50 +25,24 @@ int main(int argc, char* argv[])
 	using _STD endl;
 	using _STD cin;
 
-	__int16 ** Matrix{};
-	size_t width{35};
-	size_t height{4};
-	Show_Memory(Matrix, width, height);
+	__int16** Matrix{};
+	size_t width{};
+	size_t height{};
 	Matrix = Read_Inputs("file.in", width, height);
 	Display_Matrix(Matrix, width, height);
-	Show_Memory(Matrix, width, height);
+
+	//Hopfield(Matrix, width);	//A letter
+
+
+
 	//DEALLOCATE ALLOCATED MEMORY
 	for (size_t i = 0; i < height; ++i)
 	{
 		delete[] Matrix[i];
-		//Matrix[i] = nullptr;
 	}
 	delete[] Matrix;
-	//Matrix = nullptr;
-	Show_Memory(Matrix, width, height);
 	system("pause");
 	return 0;
-
-
-	//using _STD cout;
-	//using _STD endl;
-	//using _STD cin;
-
-	//__int16** Matrix{};
-	//size_t width{ 35 };
-	//size_t height{ 4 };
-	//Matrix = new __int16* [height];
-	//for (size_t i = 0; i < height; ++i)
-	//{
-	//	Matrix[i] = new __int16[width];
-	//	for (size_t j = 0; j < width; ++j)
-	//	{
-	//		Matrix[i][j] = NULL;
-	//	}
-	//};
-	////DEALLOCATE ALLOCATED MEMORY
-	//for (size_t i = 0; i < height; ++i)
-	//{
-	//	delete[] Matrix[i];
-	//}
-	//delete[] Matrix;
-	//system("pause");
-	//return 0;
 }
 
 __int16** Read_Inputs(const char* file_name, size_t& width, size_t& height)
@@ -74,7 +50,7 @@ __int16** Read_Inputs(const char* file_name, size_t& width, size_t& height)
 	_STD fstream file_in;
 	file_in.open("file.in", std::ios_base::in | std::ios_base::binary);
 
-	__int16 ** Matrix{};
+	__int16** Matrix{};
 
 	if (file_in.good() == false)
 	{
@@ -142,10 +118,10 @@ const void Display_Matrix(const __int16 const* const* Matrix, const size_t& widt
 				SetConsoleTextAttribute(hndl, 0x003);
 				_STD cout << "x";
 			}
-			else if(Matrix[i][j] == -1)
+			else if (Matrix[i][j] == -1)
 			{
-				SetConsoleTextAttribute(hndl, 14);
-				_STD cout << "o";
+			SetConsoleTextAttribute(hndl, 14);
+			_STD cout << "o";
 			}
 
 			//_STD cout << Matrix[i][j] << ' ';
@@ -154,4 +130,104 @@ const void Display_Matrix(const __int16 const* const* Matrix, const size_t& widt
 		_STD cout << NEW_LINE;
 	}
 	SetConsoleTextAttribute(hndl, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+}
+
+const void Display_Single_Matrix(const __int16 const* Matrix, const size_t& width)
+{
+	HANDLE hndl = GetStdHandle(STD_OUTPUT_HANDLE);
+	for (size_t i = 0; i < width; ++i)
+	{
+		if (i % 5 == 0 && i > 0)
+		{
+			_STD cout << NEW_LINE;
+		}
+		if (Matrix[i] == 1)
+		{
+			SetConsoleTextAttribute(hndl, 0x003);
+			_STD cout << "x";
+		}
+		else if (Matrix[i] == -1)
+		{
+			SetConsoleTextAttribute(hndl, 14);
+			_STD cout << "o";
+		}
+	}
+	_STD cout << NEW_LINE;
+	SetConsoleTextAttribute(hndl, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+}
+
+const void Hopfield(__int16** Matrix, const size_t& width)
+{
+	//RANDOM GENERATOR
+	std::random_device random;  //Will be used to obtain a seed for the random number engine
+	std::mt19937 generator(random()); //Standard mersenne_twister_engine seeded with rd()
+	std::uniform_real_distribution<float> dis(-1.0f, 1.0f);
+
+	float* weights{};
+	//float * neurons{};
+	weights = new float[width];
+	//neurons = new float[width];
+	//for (size_t i = 0; i < width; ++i)
+	//{
+	//	weights[i] = dis(generator);
+	//}
+
+	auto delta = [](const size_t& i, const size_t& j) -> const __int16 { if (i == j) { return 1; } else { return 0; }; };
+	int_fast16_t sum{};
+
+	bool if_break = true;
+	while (if_break)
+	{
+		//Display_Single_Matrix(Matrix, width);
+		//for (size_t i = 0; i < 7; ++i)
+		//{
+		//	for (size_t j = 0; j < 5; ++j)
+		//	{
+		//		sum += (Matrix[((i * 5) + i)] * Matrix[((i * 5) + j)]);
+		//	}
+		//}
+		//for (size_t i = 0; i < 7; ++i)
+		//{
+		//	for (size_t j = 0; j < 5; ++j)
+		//	{
+		//		weights[static_cast<size_t>((i * 5) + j)] = (1 - delta(i, j)) * sum;
+		//	}
+
+		//}
+
+
+
+
+		////for (size_t i = 0; i < width; ++i)
+		////{
+		////	if (weights[i] > 0)
+		////	{
+		////		Matrix[i] = 1;
+		////	}
+		////	else if (weights[i] < 0)
+		////	{
+		////		Matrix[i] = -1;
+		////	}
+		////}
+
+		//for (size_t k = 0; k < width; ++k)
+		//{
+		//	for (size_t i = 0; i < 7; ++i)
+		//	{
+		//		for (size_t j = 0; j < 5; ++j)
+		//		{
+		//			if (k != (i * 5) + j)
+		//			{
+		//				Matrix[k] = weights[static_cast<size_t>((i * 5) + j)] * Matrix[static_cast<size_t>((i * 5) + j)];
+		//			}
+		//		}
+		//	}
+		//}
+		//system("pause");
+		//system("cls");
+
+
+	}
+
+	delete[] weights;
 }
