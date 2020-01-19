7219661ff3078b2eecacb3c8af8920ece5810059
Damian Andrysiak <andrydamian@gmail.com>
2020-01-19 17:41:48 +0100

    working Berbasi Albert Graph Creating Algorithm

--- ADVANCED_NUMERICAL_METHODS/Graphs/Graphs/main.cpp
+++ ADVANCED_NUMERICAL_METHODS/Graphs/Graphs/main.cpp
@@ -6,6 +6,7 @@
 #include <list>
 #include <chrono>
 #include <cstdint>
+#include <algorithm>
 #include <thread>
 #include <unordered_set>
 #include <stack>
@@ -14,40 +15,11 @@
 
 #define NEW_LINE '\n'
 
-static int32_t promien{};
-static int32_t srednica{};
 
-
-
-void Set_Connections(bool Graph[100][100]);
-void Print_Graph(bool Graph[100][100]);
-//void Set_Connections_BA_Method(_STD vector<_STD list<__int32>>& people);
-//const _STD vector<_STD list<__int32>>& Set_Connections_BA_Method();
 const _STD vector<_STD list<__int32>> Set_Connections_BA_Method();
-const __int32 vertex_connections(bool Graph[100]);
 void Display_Graph(const _STD vector<_STD list<__int32>>& people);
 const float Get_CC_Coefficient(const _STD vector<_STD list<__int32>>& people);
-
-void graphBFS(const _STD vector<_STD list<__int32>> & people , int v, _STD vector<bool>& visited)
-{
-	promien = 0;
-	visited[v] = true;
-	_STD cout << v << " ";
-
-	for (auto & i : people[v])
-	{
-		if (i && !visited[i]) 
-		{
-			++promien;
-			graphBFS(people, i, visited);
-
-		}
-	}
-	if (srednica <= promien)
-	{
-		srednica = promien;
-	}
-}
+void graphBFS(const _STD vector<_STD list<__int32>>& people, int v, _STD vector<bool>& visited, _STD vector<int32_t>& way_length);
 
 int main(int argc, char* argv[])
 {
@@ -60,49 +32,24 @@ int main(int argc, char* argv[])
 	_STD cout << "CC Coefficient " << Get_CC_Coefficient(my_list) << NEW_LINE;
 
 	_STD vector<bool> visited(false);
+	_STD vector<int32_t> way_length(false);
 	visited.resize(my_list.size());
+	way_length.resize(my_list.size());
 	_STD cout << "Visited: ";
-	graphBFS(my_list, 0, visited);
+	graphBFS(my_list, 0, visited, way_length);
 	_STD cout << NEW_LINE;
-	_STD cout << "Srednica: " << srednica << NEW_LINE;
+
+	_STD cout << "Diameter: " << *std::max_element(way_length.begin(), way_length.end());
+	_STD cout << NEW_LINE;
+	_STD cout << "Radius: " << *std::max_element(way_length.begin(), std::max_element(way_length.begin(), way_length.end()));
 	_STD cout << NEW_LINE;
 
 	system("pause");
 	return 0;
 }
 
-void Set_Connections(bool Graph[100][100])
-{
-	std::random_device rd;
-	std::mt19937 gen(rd());
-	double weights[]{ 2,0,1,0 };
-	std::discrete_distribution<> d(std::begin(weights), std::end(weights));
-	for (size_t i = 0; i < 100; ++i)
-	{
-		for (size_t j = 0; j < 100; ++j)
-		{
-			Graph[i][j] = d(gen);
-		}
-	}
-}
-
-void Print_Graph(bool Graph[100][100])
-{
-	for (size_t i = 0; i < 100; ++i)
-	{
-		for (size_t j = 0; j < 100; ++j)
-		{
-			_STD cout << Graph[i][j] << " ";
-		}
-		_STD cout << NEW_LINE;
-	}
-}
-
-//void Set_Connections_BA_Method(_STD vector<_STD list<__int32>>& people)
-//const _STD vector<_STD list<__int32>>& Set_Connections_BA_Method()
 const _STD vector<_STD list<__int32>> Set_Connections_BA_Method()
 {
-	//using namespace std::chrono_literals;
 	_STD mt19937 generator(time(nullptr));
 	////Initialize basic graph with 2 connections
 	////ki -> amout of connections at each vertex
@@ -110,9 +57,6 @@ const _STD vector<_STD list<__int32>> Set_Connections_BA_Method()
 
 	const size_t N{20};
 	//basic initialization
-	//people.resize(2);
-	//people[0].push_back(1);
-	//people[1].push_back(0);
 	_STD vector<_STD list<__int32>> people(2);
 	//basic initialization
 	people.resize(2);
@@ -159,20 +103,6 @@ const _STD vector<_STD list<__int32>> Set_Connections_BA_Method()
 	return people;
 }
 
-const __int32 vertex_connections(bool Graph[100])
-{
-	__int32 connections{};
-
-	for (size_t i = 0; i < 100; ++i)
-	{
-		if (Graph[i] == true)
-		{
-			connections++;
-		}
-	}
-	return connections;
-}
-
 void Display_Graph(const _STD vector<_STD list<__int32>>& people)
 {
 	for (size_t i = 0; i < people.size(); ++i)
@@ -190,17 +120,31 @@ const float Get_CC_Coefficient(const _STD vector<_STD list<__int32>>& people)
 {
 	float CC_Coefficient{};
 	uint32_t amount_of_edges{};
-	float kc{};
 	float temp{};
 
 	for (size_t i = 0; i < people.size(); ++i)
 	{
 		amount_of_edges += people[i].size();
 	}
-	//kc = static_cast<float>(0.5f * amount_of_edges);
 	temp = static_cast<float>(((people.size() * (people.size() - 1)) / 2));
 
 	CC_Coefficient = static_cast<float>(amount_of_edges /temp);
 
 	return float(CC_Coefficient);
+}
+
+void graphBFS(const::std::vector<::std::list<__int32>>& people, int v, ::std::vector<bool>& visited, ::std::vector<int32_t>& way_length)
+{
+	visited[v] = true;
+	_STD cout << v << " ";
+
+	for (auto& i : people[v])
+	{
+		if (i && visited[i] == false)
+		{
+			way_length[v]++;
+			graphBFS(people, i, visited, way_length);
+
+		}
+	}
 }
