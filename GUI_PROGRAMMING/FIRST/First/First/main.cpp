ed97e5db0cf99e6caaa8b53da43db895bdabf9b1
Damian Andrysiak <andrydamian@gmail.com>
2020-03-08 12:10:44 +0100

    up

--- GUI_PROGRAMMING/FIRST/First/First/main.cpp
+++ GUI_PROGRAMMING/FIRST/First/First/main.cpp
@@ -99,7 +99,6 @@ int main()
             wcin.getline(wtxt, 1024);
             //SetWindowText(edit, wtxt);
             SendMessage(edit, WM_SETTEXT, 0, (LPARAM)wtxt);
-
             break;
 
         case 9:     //Save
@@ -111,6 +110,7 @@ int main()
 
             HWND savedlg;
             savedlg = FindWindow(NULL, L"Save As");
+            //HWND hwnd_edit = FindWindow("Edit"
             cout << (savedlg ? "sukcess!" : "failure") << endl;
             cout << "Save dialog handle: " << hex << savedlg << endl;
 
@@ -118,14 +118,133 @@ int main()
             ///////////////////////////////////////////////////////////////
 
             HWND savedlg1;
-            savedlg1 = FindWindowEx(NULL, savedlg, L"DirectUIHWND");
+            //savedlg1 = FindWindowEx(savedlg, NULL, L"(Dialog)", NULL);
+            //savedlg1 = FindWindow(NULL, L"#32770");
+            savedlg1 = FindWindowEx(savedlg, NULL, L"ToolbarWindow32", NULL);
+
+
+            //HWND saving_title;
+            ////savedlg1 = FindWindowEx(savedlg, NULL, L"(Dialog)", NULL);
+            ////savedlg1 = FindWindow(NULL, L"#32770");
+            //saving_title = FindWindowEx(savedlg1, NULL, L"DirectUIHWND", NULL);
+            //saving_title = FindWindow(NULL, L"*.txt");
+           // FindWindowEx()
             cout << (savedlg1 ? "sukcess!" : "failure") << endl;
-            cout << "Save dialog handle: " << hex << savedlg << endl;
+           // cout << (saving_title ? "sukcess!" : "failure") << endl;
+            cout << "Save dialog handle: " << hex << savedlg1 << endl;
+            //cout << "Save dialog handle: " << hex << saving_title << endl;
           /*  cout << "enter text: ";
             wcin.getline(wtxt, 1024);*/
-            wchar_t disk_name[] = { 'C',':','/' };
+            //wchar_t disk_name[] = { 'C',':','/' };
+            wchar_t disk_name[] = L"Address: Cddd";
+            //std::cin.get();
+            //SetFileInformationByHandle(savedlg,)
+            //OPENFILENAMEA file{};
+            //GetOpenFileNameA(&file);
+            //TCHAR tab[1024]{};
+            //GetCurrentDirectory(sizeof(tab), tab);
+            //ShowWindow(savedlg, SW_HIDE);
+            //SendMessage(savedlg1, WM_SETTEXT,0, (LPARAM)L"Address: C:\\Users\\andry\\Desktop\\Dora");
+            ///SetWindowTextA(savedlg1, (LPCSTR)disk_name);
+            //SetWindowText(savedlg1, disk_name);
+
+
+
+
+            //OPENFILENAME ofn;
+            //wchar_t szFileName[MAX_PATH]{};
+
+            //ZeroMemory(&ofn, sizeof(ofn));
+
+            //ofn.lStructSize = sizeof(ofn); // SEE NOTE BELOW
+            //ofn.hwndOwner = savedlg;
+            //ofn.lpstrFilter = L"Text Files (*.txt)\0*.txt\0All Files (*.*)\0*.*\0";
+            //ofn.lpstrFile = szFileName;
+            //ofn.nMaxFile = MAX_PATH;
+            //ofn.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
+            //ofn.lpstrDefExt = L"txt";
+
+            //if (GetOpenFileName(&ofn))
+            //{
+            //    // Do something usefull with the filename stored in szFileName 
+            //}
+            //HANDLE hFile;
+            //BOOL bSuccess = FALSE;
+
+            //hFile = CreateFile(L"text.txt", GENERIC_WRITE, 0, NULL,
+            //    CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+            //if (hFile != INVALID_HANDLE_VALUE)
+            //{
+            //    DWORD dwTextLength;
+
+            //    dwTextLength = GetWindowTextLength(savedlg);
+            //    // No need to bother if there's no text.
+            //    if (dwTextLength > 0)
+            //    {
+            //        LPSTR pszText;
+            //        DWORD dwBufferSize = dwTextLength + 1;
+
+            //        pszText = (LPSTR)GlobalAlloc(GPTR, dwBufferSize);
+            //        if (pszText != NULL)
+            //        {
+            //            if (GetWindowText(savedlg,LPWSTR(pszText), dwBufferSize))
+            //            {
+            //                DWORD dwWritten;
+
+            //                if (WriteFile(hFile, pszText, dwTextLength, &dwWritten, NULL))
+            //                    bSuccess = TRUE;
+            //            }
+            //            GlobalFree(pszText);
+            //        }
+            //    }
+            //    CloseHandle(hFile);
+            //}
+
+            //OPENFILENAME ofn{};
+            //char file_name[100]{};
+
+            //ZeroMemory(&ofn, sizeof(OPENFILENAME));
+
+            //ofn.lStructSize = sizeof(OPENFILENAME);
+            //ofn.hwndOwner = savedlg1;
+            //ofn.lpstrFile = (LPWSTR)file_name;
+            //ofn.lpstrFile[0] = '\0';
+            //ofn.nMaxFile = 100;
+            //ofn.lpstrFilter = L"C:\\Users\\andry\\Desktop\\Dora";
+            //ofn.nFilterIndex = 1;
+
+            //GetSaveFileName(&ofn);
+            //GetWindowText(savedlg1, (LPWSTR)file_name, 100);
+
+ /*           DWORD dwTextLength = GetWindowTextLength(savedlg1) + 1;
+
+            if (dwTextLength > 0)
+
+            {
+
+                DWORD dwBufferSize = dwTextLength + 1;
+
+                LPWSTR pszText = (LPWSTR)GlobalAlloc(GPTR, dwBufferSize * sizeof(LPWSTR));
+
+                if (pszText != NULL)
+
+                {
+
+                    GetWindowText(savedlg1, pszText, dwTextLength);
+
+                    SetWindowText(savedlg1, L"DDDDDD");
+
+                    GlobalFree(pszText);
+
+                }
+
+            }*/
+            SendMessage(edit, WM_GETTEXT, 0, (LPARAM)wtxt);
+            std::cin.get();
+            //ShowWindow(savedlg, SW_SHOW);
+
             //SetWindowText(edit, wtxt);
-           // SendMessage(edit, WM_SETTEXT, 0, (LPARAM)disk_name+(LPARAM)wtxt);
+            //SendMessage(edit, WM_SETTEXT, 0, (LPARAM)disk_name+(LPARAM)wtxt);
             break;
         }
     }
