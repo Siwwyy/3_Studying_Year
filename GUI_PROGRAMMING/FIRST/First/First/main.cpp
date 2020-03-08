58596d3ee0c41f30d259d6a09c29b4eb69da99d7
Damian Andrysiak <andrydamian@gmail.com>
2020-03-08 20:54:02 +0100

    working saving file and save as file

--- GUI_PROGRAMMING/FIRST/First/First/main.cpp
+++ GUI_PROGRAMMING/FIRST/First/First/main.cpp
@@ -102,212 +102,73 @@ int main()
             break;
 
         case 9:     //Save
-            SendMessage(start, WM_COMMAND, 3, 0);
+            char title[1024];
+            GetWindowTextA(start, (LPSTR)title, 1024);
+            if ((title[0] == 'U' && title[1] == 'n') || (title[0] == '*' && title[1] == 'U'))
+            {
+                PostMessage(start, WM_COMMAND, 4, 0);
+                Sleep(1000);
+
+                wchar_t file_path[1024];
+                HWND SaveAs = 0;
+                HWND View = 0;
+                HWND Direct = 0;
+                HWND FloatNotifySink = 0;
+                HWND ComboBox = 0;
+                HWND file_path_field = 0;
+
+                SaveAs = FindWindow(NULL, L"Save As");
+                View = FindWindowEx(SaveAs, NULL, L"DUIViewWndClassName", NULL);
+                Direct = FindWindowEx(View, NULL, L"DirectUIHWND", NULL);
+                FloatNotifySink = FindWindowEx(Direct, NULL, L"FloatNotifySink", NULL);
+                ComboBox = FindWindowEx(FloatNotifySink, NULL, L"ComboBox", NULL);
+                file_path_field = FindWindowEx(ComboBox, NULL, L"Edit", NULL);
+                cout << "Set file name: ";
+                wcin.getline(file_path, 1024);
+                std::cout << '\n';
+                SendMessage(file_path_field, WM_SETTEXT, 0, (LPARAM)file_path);
+                HWND child = ::FindWindowEx(SaveAs,NULL,TEXT("Button"), NULL);
+
+                if (child)
+                {
+                    ::SendMessage(child, BM_CLICK, NULL, NULL);
+                }
+            }
+            else
+            {
+                SendMessage(start, WM_COMMAND, 3, 0);
+                Sleep(1000);
+
+            }
             break;
+
         case 10:    //Save as
             PostMessage(start, WM_COMMAND, 4, 0);
             Sleep(1000);
 
-            HWND savedlg;
-            savedlg = FindWindow(NULL, L"Save As");
-            //HWND hwnd_edit = FindWindow("Edit"
-            cout << (savedlg ? "sukcess!" : "failure") << endl;
-            cout << "Save dialog handle: " << hex << savedlg << endl;
+            wchar_t file_path[1024];
+            HWND SaveAs = 0;
+            HWND View = 0;
+            HWND Direct = 0;
+            HWND FloatNotifySink = 0;
+            HWND ComboBox = 0;
+            HWND file_path_field = 0;
 
-
-            ///////////////////////////////////////////////////////////////
-
-            HWND savedlg1;
-            //savedlg1 = FindWindowEx(savedlg, NULL, L"(Dialog)", NULL);
-            //savedlg1 = FindWindow(NULL, L"#32770");
-            savedlg1 = FindWindowEx(savedlg, NULL, L"ToolbarWindow32", NULL);
-
-
-            //HWND saving_title;
-            ////savedlg1 = FindWindowEx(savedlg, NULL, L"(Dialog)", NULL);
-            ////savedlg1 = FindWindow(NULL, L"#32770");
-            //saving_title = FindWindowEx(savedlg1, NULL, L"DirectUIHWND", NULL);
-            //saving_title = FindWindow(NULL, L"*.txt");
-           // FindWindowEx()
-            cout << (savedlg1 ? "sukcess!" : "failure") << endl;
-           // cout << (saving_title ? "sukcess!" : "failure") << endl;
-            cout << "Save dialog handle: " << hex << savedlg1 << endl;
-            //cout << "Save dialog handle: " << hex << saving_title << endl;
-          /*  cout << "enter text: ";
-            wcin.getline(wtxt, 1024);*/
-            //wchar_t disk_name[] = { 'C',':','/' };
-            wchar_t disk_name[] = L"Address: Cddd";
-            //std::cin.get();
-            //SetFileInformationByHandle(savedlg,)
-            //OPENFILENAMEA file{};
-            //GetOpenFileNameA(&file);
-            //TCHAR tab[1024]{};
-            //GetCurrentDirectory(sizeof(tab), tab);
-            //ShowWindow(savedlg, SW_HIDE);
-            //SendMessage(savedlg1, WM_SETTEXT,0, (LPARAM)L"Address: C:\\Users\\andry\\Desktop\\Dora");
-            ///SetWindowTextA(savedlg1, (LPCSTR)disk_name);
-            //SetWindowText(savedlg1, disk_name);
-
-
-
-
-            //OPENFILENAME ofn;
-            //char szFileName[MAX_PATH]{};
-
-            //ZeroMemory(&ofn, sizeof(ofn));
-
-            //ofn.lStructSize = sizeof(ofn); // SEE NOTE BELOW
-            //ofn.hwndOwner = savedlg;
-            //ofn.lpstrFilter = L"Text Files (*.txt)\0*.txt\0All Files (*.*)\0*.*\0";
-            //ofn.lpstrFile = (LPWSTR)szFileName;
-            //ofn.nMaxFile = MAX_PATH;
-            ////ofn.Flags = OFN_
-            ////ofn.lpstrDefExt = L"txt";
-
-            //if (GetSaveFileName(&ofn))
-            //{
-            //    // Do something usefull with the filename stored in szFileName 
-            //}
-            //HANDLE hFile;
-            //BOOL bSuccess = FALSE;
-
-            //hFile = CreateFile(L"text.txt", GENERIC_WRITE, 0, NULL,
-            //    CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-            //if (hFile != INVALID_HANDLE_VALUE)
-            //{
-            //    DWORD dwTextLength;
-
-            //    dwTextLength = GetWindowTextLength(savedlg);
-            //    // No need to bother if there's no text.
-            //    if (dwTextLength > 0)
-            //    {
-            //        LPSTR pszText;
-            //        DWORD dwBufferSize = dwTextLength + 1;
-
-            //        pszText = (LPSTR)GlobalAlloc(GPTR, dwBufferSize);
-            //        if (pszText != NULL)
-            //        {
-            //            if (GetWindowText(savedlg,LPWSTR(pszText), dwBufferSize))
-            //            {
-            //                DWORD dwWritten;
-
-            //                if (WriteFile(hFile, pszText, dwTextLength, &dwWritten, NULL))
-            //                    bSuccess = TRUE;
-            //            }
-            //            GlobalFree(pszText);
-            //        }
-            //    }
-            //    CloseHandle(hFile);
-            //}
-
-            //OPENFILENAME ofn{};
-            //char file_name[100]{};
-
-            //ZeroMemory(&ofn, sizeof(OPENFILENAME));
-
-            //ofn.lStructSize = sizeof(OPENFILENAME);
-            //ofn.hwndOwner = savedlg1;
-            //ofn.lpstrFile = (LPWSTR)file_name;
-            //ofn.lpstrFile[0] = '\0';
-            //ofn.nMaxFile = 100;
-            //ofn.lpstrFilter = L"C:\\Users\\andry\\Desktop\\Dora";
-            //ofn.nFilterIndex = 1;
-
-            //GetSaveFileName(&ofn);
-            //GetWindowText(savedlg1, (LPWSTR)file_name, 100);
-
- /*           DWORD dwTextLength = GetWindowTextLength(savedlg1) + 1;
-
-            if (dwTextLength > 0)
-
+            SaveAs = FindWindow(NULL, L"Save As");
+            View = FindWindowEx(SaveAs, NULL, L"DUIViewWndClassName", NULL);
+            Direct = FindWindowEx(View, NULL, L"DirectUIHWND", NULL);
+            FloatNotifySink = FindWindowEx(Direct, NULL, L"FloatNotifySink", NULL);
+            ComboBox = FindWindowEx(FloatNotifySink, NULL, L"ComboBox", NULL);
+            file_path_field = FindWindowEx(ComboBox, NULL, L"Edit", NULL);
+            cout << "Set file name: ";
+            wcin.getline(file_path, 1024);
+            std::cout << '\n';
+            SendMessage(file_path_field, WM_SETTEXT, 0, (LPARAM)file_path);
+            HWND child = ::FindWindowEx(SaveAs, NULL, TEXT("Button"), NULL);
+            if (child)
             {
-
-                DWORD dwBufferSize = dwTextLength + 1;
-
-                LPWSTR pszText = (LPWSTR)GlobalAlloc(GPTR, dwBufferSize * sizeof(LPWSTR));
-
-                if (pszText != NULL)
-
-                {
-
-                    GetWindowText(savedlg1, pszText, dwTextLength);
-
-                    SetWindowText(savedlg1, L"DDDDDD");
-
-                    GlobalFree(pszText);
-
-                }
-
-            }*/
-            //int slength = SendMessage(savedlg1, WM_GETTEXTLENGTH, NULL,1);
-            ////SendMessage(savedlg1, WM_GETTEXT, 0, (LPARAM)wtxt);
-            //SendMessage(savedlg1, WM_GETTEXT, slength, (LPARAM)wtxt);
-            //char filename[MAX_PATH];
-
-            //OPENFILENAME ofn;
-            //ZeroMemory(&filename, sizeof(filename));
-            //ZeroMemory(&ofn, sizeof(ofn));
-            //ofn.lStructSize = sizeof(ofn);
-            //ofn.hwndOwner = savedlg;  // If you have a window to center over, put its HANDLE here
-            //ofn.lpstrFilter = L"Text Files\0*.txt\0Any File\0*.*\0";
-            //ofn.lpstrFile = (LPWSTR)filename;
-            //ofn.nMaxFile = MAX_PATH;
-            //ofn.lpstrTitle = L"Save As";
-            ////ofn.Flags = OFN_DONTADDTORECENT | OFN_FILEMUSTEXIST;
-
-            //if (GetSaveFileName(&ofn))
-            //{
-            //    std::cout << "You chose the file \"" << filename << "\"\n";
-            //}
-            //else
-            //{
-            //    // All this stuff below is to tell you exactly how you messed up above. 
-            //    // Once you've got that fixed, you can often (not always!) reduce it to a 'user cancelled' assumption.
-            //    switch (CommDlgExtendedError())
-            //    {
-            //    case CDERR_DIALOGFAILURE: std::cout << "CDERR_DIALOGFAILURE\n";   break;
-            //    case CDERR_FINDRESFAILURE: std::cout << "CDERR_FINDRESFAILURE\n";  break;
-            //    case CDERR_INITIALIZATION: std::cout << "CDERR_INITIALIZATION\n";  break;
-            //    case CDERR_LOADRESFAILURE: std::cout << "CDERR_LOADRESFAILURE\n";  break;
-            //    case CDERR_LOADSTRFAILURE: std::cout << "CDERR_LOADSTRFAILURE\n";  break;
-            //    case CDERR_LOCKRESFAILURE: std::cout << "CDERR_LOCKRESFAILURE\n";  break;
-            //    case CDERR_MEMALLOCFAILURE: std::cout << "CDERR_MEMALLOCFAILURE\n"; break;
-            //    case CDERR_MEMLOCKFAILURE: std::cout << "CDERR_MEMLOCKFAILURE\n";  break;
-            //    case CDERR_NOHINSTANCE: std::cout << "CDERR_NOHINSTANCE\n";     break;
-            //    case CDERR_NOHOOK: std::cout << "CDERR_NOHOOK\n";          break;
-            //    case CDERR_NOTEMPLATE: std::cout << "CDERR_NOTEMPLATE\n";      break;
-            //    case CDERR_STRUCTSIZE: std::cout << "CDERR_STRUCTSIZE\n";      break;
-            //    case FNERR_BUFFERTOOSMALL: std::cout << "FNERR_BUFFERTOOSMALL\n";  break;
-            //    case FNERR_INVALIDFILENAME: std::cout << "FNERR_INVALIDFILENAME\n"; break;
-            //    case FNERR_SUBCLASSFAILURE: std::cout << "FNERR_SUBCLASSFAILURE\n"; break;
-            //    default: std::cout << "You cancelled.\n";
-            //    }
-            //}
-            wchar_t temp[1024];
-            HWND tempWindowHandle = 0;
-            HWND tempView = 0;
-            HWND tempDirect = 0;
-            HWND tempFloatNotify = 0;
-            HWND tempCombo = 0;
-            HWND tempField = 0;
-
-
-
-            PostMessage(start, WM_COMMAND, 4, 0);
-            tempWindowHandle = FindWindow(NULL, L"Save As");
-            tempView = FindWindowEx(tempWindowHandle, NULL, L"DUIViewWndClassName", NULL);
-            tempDirect = FindWindowEx(tempView, NULL, L"DirectUIHWND", NULL);
-            tempFloatNotify = FindWindowEx(tempDirect, NULL, L"FloatNotifySink", NULL);
-            tempCombo = FindWindowEx(tempFloatNotify, NULL, L"ComboBox", NULL);
-            tempField = FindWindowEx(tempCombo, NULL, L"Edit", NULL);
-            cout << "Edit handle: " << hex << tempField << endl;
-            wcin.getline(wtxt, 1024);
-            SendMessage(tempField, WM_SETTEXT, 0, (LPARAM)wtxt);
-            std::cin.get();
-            //ShowWindow(savedlg, SW_SHOW);
-
-            //SetWindowText(edit, wtxt);
-            //SendMessage(edit, WM_SETTEXT, 0, (LPARAM)disk_name+(LPARAM)wtxt);
+                ::SendMessage(child, BM_CLICK, NULL, NULL);
+            }
             break;
         }
     }
