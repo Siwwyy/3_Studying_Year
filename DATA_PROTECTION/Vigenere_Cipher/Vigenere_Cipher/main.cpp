aa3e6540b121990d14997f38828394ae0a684b8c
Damian Andrysiak <andrydamian@gmail.com>
2020-03-09 21:57:23 +0100

    bug fixing

--- DATA_PROTECTION/Vigenere_Cipher/Vigenere_Cipher/main.cpp
+++ DATA_PROTECTION/Vigenere_Cipher/Vigenere_Cipher/main.cpp
@@ -13,7 +13,8 @@ void Load_Data(std::string & Data, const std::string file_name);
 void Save_Data(std::string & Data, const std::string file_name);
 void Initialize_Alphabet(std::vector<std::vector<char>> & Alphabet);
 void Print_Alphabet(std::vector<std::vector<char>> & Alphabet);
-void Encrypt(std::vector<std::vector<char>> & Alphabet, std::string & Data, std::string Key);
+void Encrypt(std::vector<std::vector<char>> & Alphabet, std::string & Data, std::string& Key);
+void Decrypt(std::vector<std::vector<char>> & Alphabet, std::string & Data, std::string& Key);
 
 const std::string our_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
@@ -21,12 +22,16 @@ int main(int argc, char* argv[])
 {
 	std::string Data{};
 	std::vector<std::vector<char>> Alphabet(our_alphabet.size());
+	//std::string Key = "9AB";
 	std::string Key = "BCDE";
 	Load_Data(Data, "file.in");
+	//Load_Data(Data, "file.out"); //out means is ready for decrypt
 	Initialize_Alphabet(Alphabet);
 	//Print_Alphabet(Alphabet);
 	Encrypt(Alphabet, Data, Key);
+	//Decrypt(Alphabet, Data, Key);
 	Save_Data(Data, "file.out");
+	//Save_Data(Data, "file_decrypted.out");
 	system("pause");
 	return 0;
 }
@@ -74,9 +79,59 @@ void Print_Alphabet(std::vector<std::vector<char>>& Alphabet)
 	}
 }
 
-void Encrypt(std::vector<std::vector<char>>& Alphabet, std::string& Data, std::string Key)
+void Encrypt(std::vector<std::vector<char>>& Alphabet, std::string& Data, std::string & Key)
 {
-	auto Correct_Sign = [&](const char & sign) -> bool
+	auto Correct_Sign = [&](char & sign) -> bool
+	{
+		int8_t sign_value = static_cast<int8_t>(sign);
+		if (sign_value >= 97 && sign_value <= 122)
+		{
+			sign_value -= 32;
+
+		}
+
+		if ((sign_value >= 48 && sign_value <= 57))
+		{
+			sign = static_cast<char>(sign_value);
+			return true;
+		}
+		else if (sign_value >= 65 && sign_value <= 90)
+		{
+			sign = static_cast<char>(sign_value);
+			return true;
+		}
+		return false;
+	};
+	std::string::iterator string_iterator_key = Key.begin();
+	for (typename std::string::iterator string_iterator = Data.begin(); string_iterator != Data.end(); ++string_iterator)
+	{
+		if (Correct_Sign(*string_iterator) == true)
+		{
+			if (string_iterator_key != Key.end())
+			{
+				std::vector<char>::iterator it = std::find(Alphabet[0].begin(), Alphabet[0].end(), *string_iterator);
+				std::vector<char>::iterator it_second = std::find(Alphabet[0].begin(), Alphabet[0].end(), *string_iterator_key);
+
+				const size_t position_data = std::distance(Alphabet[0].begin(), it);
+				const size_t position_key = std::distance(Alphabet[0].begin(), it_second);
+
+				//std::cout << *it << " -> " << Alphabet[position_key][position_data] << '\n';
+				//std::cin.get();
+				//*string_iterator = it_second[position];
+				*string_iterator = Alphabet[position_key][position_data];
+				++string_iterator_key;
+			}
+			if (string_iterator_key == Key.end())
+			{
+				string_iterator_key = Key.begin();
+			}
+		}
+	}
+}
+
+void Decrypt(std::vector<std::vector<char>>& Alphabet, std::string& Data, std::string& Key)
+{
+	auto Correct_Sign = [&](const char& sign) -> bool
 	{
 		int8_t sign_value = static_cast<int8_t>(sign);
 		if (sign_value >= 97 && sign_value <= 122)
@@ -104,7 +159,6 @@ void Encrypt(std::vector<std::vector<char>>& Alphabet, std::string& Data, std::s
 				std::vector<char>::iterator it = std::find(Alphabet[0].begin(), Alphabet[0].end(), *string_iterator);
 				const size_t position = std::distance(Alphabet[0].begin(), it);
 				std::vector<char>::iterator it_second = std::find(Alphabet[0].begin(), Alphabet[0].end(), *string_iterator_key);
-				//std::cout << *it << " -> " << *it_second << '\n';
 				*string_iterator = it_second[position];
 				++string_iterator_key;
 			}
