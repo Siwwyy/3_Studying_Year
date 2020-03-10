1e51f2a995278cd0a8f2cfb2f2686c7227f59835
Damian Andrysiak <andrydamian@gmail.com>
2020-03-10 20:30:45 +0100

    up

---
+++ CONCURRENCY_PROGRAMMING_ASP_NET/TUTORIAL/First/Fourth/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+
+namespace Fourth
+{
+    class ThreadCreationProgram
+    {
+        public static void CallToChildThread()
+        {
+            try
+            {
+                Console.WriteLine("Child thread starts");
+
+                // do some work, like counting to 10
+                for (int counter = 0; counter <= 10; counter++)
+                {
+                    Thread.Sleep(500);
+                    Console.WriteLine(counter);
+                }
+
+                Console.WriteLine("Child Thread Completed");
+            }
+            catch (ThreadAbortException e)
+            {
+                Console.WriteLine("Thread Abort Exception");
+            }
+            finally
+            {
+                Console.WriteLine("Couldn't catch the Thread Exception");
+            }
+        }
+        static void Main(string[] args)
+        {
+            ThreadStart childref = new ThreadStart(CallToChildThread);
+            Console.WriteLine("In Main: Creating the Child thread");
+
+            Thread childThread = new Thread(childref);
+            childThread.Start();
+
+            //stop the main thread for some time
+            Thread.Sleep(2000);
+
+            //now abort the child
+            Console.WriteLine("In Main: Aborting the Child thread");
+
+            childThread.Abort();
+            Console.ReadKey();
+        }
+    }
+}
