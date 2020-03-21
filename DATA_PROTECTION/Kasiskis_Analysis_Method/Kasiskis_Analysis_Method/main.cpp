3a24344335c0cd5a1cda73a433e4f848ee088ea5
Damian Andrysiak <andrydamian@gmail.com>
2020-03-21 13:04:59 +0100

    update, Kasinskis Method

--- DATA_PROTECTION/Kasiskis_Analysis_Method/Kasiskis_Analysis_Method/main.cpp
+++ DATA_PROTECTION/Kasiskis_Analysis_Method/Kasiskis_Analysis_Method/main.cpp
@@ -28,6 +28,7 @@ int main(int argc, char* argv[])
 
 
 	Load_Data(Data, "file.in");
+	//Load_Data(Data, "file2.in");
 	//Print_Data(Data);
 
 	Find_Occurences(Data, Occurences, minimal_seeking_length);
@@ -117,29 +118,92 @@ void Print_Data(const std::string& Data)
 void Find_Occurences(const std::string& Data, std::string& Occurences, size_t minimal_seeking_length)
 {
 	std::string sub_string{};
-	while(minimal_seeking_length != Data.length())
+	auto length_case = [](const size_t& _Left, const size_t _Right) -> bool
+	{
+		return _Left != _Right;
+	};
+
+	std::set<std::vector<std::string>> Temp{};
+	std::vector<std::string> Temp_Second{};
+
+	while(length_case(minimal_seeking_length,Data.length() + 1))
 	{
 		//std::cout << minimal_seeking_length << ' ' << Data.length() << '\n';
-		for (typename std::string::const_iterator string_iterator = Data.begin(); string_iterator != Data.end(); ++string_iterator)
+		std::string temp = Data;
+		std::string replacer(temp);
+		for (size_t i = 0; i < minimal_seeking_length; ++i)
 		{
-			std::string second_sub_string{};
-			const size_t current_position = std::distance(Data.begin(), string_iterator);
-			sub_string = Data.substr(current_position, minimal_seeking_length);
-			//std::cout << sub_string << '\n';
-			//std::cout << current_position << ' ' << current_position + minimal_seeking_length << ' ' << sub_string << '\n';
-			for (typename std::string::const_iterator string_iterator_third = string_iterator; string_iterator_third != Data.end(); ++string_iterator_third)
+			replacer[i] = '#';
+		}
+		for (typename std::string::iterator string_iterator = temp.begin(); string_iterator != temp.end(); ++string_iterator)
+		{
+			const size_t current_position = std::distance(temp.begin(), string_iterator);
+			if (length_case(static_cast<size_t>(current_position + minimal_seeking_length), Data.length() + 1))
 			{
 				std::string second_sub_string{};
-				const size_t second_current_position = std::distance(Data.begin(), string_iterator_third);
-				second_sub_string = Data.substr(second_current_position, minimal_seeking_length);
-				if (sub_string == second_sub_string && second_sub_string.length() == minimal_seeking_length)
+				sub_string = temp.substr(current_position, minimal_seeking_length);
+				//std::cout << sub_string << '\n';
+				//std::cout << current_position << ' ' << current_position + minimal_seeking_length << ' ' << sub_string << '\n';
+				if (sub_string[0] != '#')
 				{
-					std::cout << sub_string << '\n';
+					//std::cout << sub_string << '\n';
+					for (typename std::string::iterator string_iterator_third = string_iterator + 1; string_iterator_third != temp.end(); ++string_iterator_third)
+					{
+						const size_t second_current_position = std::distance(temp.begin(), string_iterator_third);
+						if (length_case(static_cast<size_t>(second_current_position + minimal_seeking_length), Data.length() + 1))
+						{
+							std::string second_sub_string{};
+							second_sub_string = temp.substr(second_current_position, minimal_seeking_length);
+							if (sub_string == second_sub_string)
+							{
+								Temp_Second.emplace_back(sub_string);
+								if (static_cast<size_t>(minimal_seeking_length + second_current_position) <= temp.length() && second_current_position <= temp.length())
+								{
+									for (size_t i = second_current_position; i < static_cast<size_t>(minimal_seeking_length + second_current_position); ++i)
+									{
+										//temp.replace(second_current_position, static_cast<size_t>(minimal_seeking_length + second_current_position), replacer);
+										temp[i] = '#';
+									}
+								}
+								//if (sub_string == replacer)
+								//{
+								//	std::cout << sub_string;
+								//}
+								//std::cout << sub_string << '\n';
+								//std::cout << temp;
+								//std::cin.get();
+							}
+						}
+						else
+						{
+							if (Temp_Second.size() > 0)
+							{
+								Temp_Second.emplace_back(Temp_Second[0]);
+								Temp.insert(Temp_Second);
+								Temp_Second.clear();
+							}
+							break;
+						}
+					}
 				}
-				/*std::cout << second_sub_string << '\n';*/
 			}
-			std::cin.get();
+			else
+			{
+				break;
+			}			
 		}
 		++minimal_seeking_length;
 	}
+
+
+	std::cout << "|================================|" << '\n';
+	for (typename std::set<std::vector<std::string>>::const_iterator vec_iterator = Temp.begin(); vec_iterator != Temp.end(); ++vec_iterator)
+	{
+		for (typename std::vector<std::string>::const_iterator vec_iterator_second = vec_iterator->begin(); vec_iterator_second != vec_iterator->end(); ++vec_iterator_second)
+		{
+			std::cout << *vec_iterator_second << ' ';
+		}
+		std::cout << '\n';
+	}
+	std::cout << "|================================|" << '\n';
 }
