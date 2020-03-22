9f5c4286afedd0bcd0e7a3d18510d991f6350d65
Damian Andrysiak <andrydamian@gmail.com>
2020-03-22 17:09:44 +0100

    up

--- DATA_PROTECTION/Kasiskis_Analysis_Method/Kasiskis_Analysis_Method/main.cpp
+++ DATA_PROTECTION/Kasiskis_Analysis_Method/Kasiskis_Analysis_Method/main.cpp
@@ -23,11 +23,11 @@ int main(int argc, char* argv[])
 	using std::cin;
 
 	std::string Data{};
-	const size_t minimal_seeking_length{ 3 };
+	const size_t minimal_seeking_length{ 5 };
 
 
-	Load_Data(Data, "file.in");
-	//Load_Data(Data, "Out.out");
+	//Load_Data(Data, "file.in");
+	Load_Data(Data, "Out.out");
 	//Load_Data(Data, "file_big.in");
 	//Load_Data(Data, "file2.in");
 	//Print_Data(Data);
@@ -118,119 +118,43 @@ void Print_Data(const std::string& Data)
 
 void Find_Occurences(const std::string& Data, size_t minimal_seeking_length)
 {
-	auto length_case = [](const size_t& _Left, const size_t _Right) -> bool
-	{
-		return _Left != _Right;
-	};
 
-	std::vector<std::vector<std::pair<std::string, size_t>>> Temp{};
-	std::vector<std::pair<std::string, size_t>> Temp_Second{};
-	std::set<std::string> visitied{};
+	//std::vector<std::vector<std::pair<std::string, size_t>>> Temp{};
+	//std::vector<std::pair<std::string, size_t>> Temp_Second{};
+	//std::set<std::string> visitied{};
 
 	std::map<std::string, std::set<int32_t>> Occurences;
 	std::string sub_string{};
-	std::string second_sub_string{};
-	//while(length_case(minimal_seeking_length,static_cast<size_t>((Data.length() + 1)/2)))
+	
 	while(minimal_seeking_length <= 20)
 	{
-		/*for (typename std::string::const_iterator string_iterator = Data.begin(); string_iterator != Data.end(); ++string_iterator)
+		for (size_t i = 0; i < Data.size(); ++i)
 		{
-			const size_t current_position = std::distance(Data.begin(), string_iterator);
-			if (length_case(static_cast<size_t>(current_position + minimal_seeking_length), Data.length() + 1))
+			sub_string = Data.substr(i, minimal_seeking_length);
+			if (sub_string.size() == minimal_seeking_length)
 			{
-				std::string sub_string{};
-				sub_string = Data.substr(current_position, minimal_seeking_length);
-				for (typename std::string::const_iterator string_iterator_third = string_iterator; string_iterator_third != Data.end(); ++string_iterator_third)
-				{
-					const size_t second_current_position = std::distance(Data.begin(), string_iterator_third);
-					if (length_case(static_cast<size_t>(second_current_position + minimal_seeking_length), Data.length() + 1))
-					{
-						std::string second_sub_string{};
-						second_sub_string = Data.substr(second_current_position, minimal_seeking_length);
-						if (second_sub_string == sub_string)
-						{
-							Occurences[sub_string].insert(second_current_position);
-						}
-					}
-				}
-			}
-		}*/
-		//for (typename std::string::const_iterator string_iterator = Data.begin(); string_iterator != Data.end(); ++string_iterator)
-		for (size_t i = 0; i < Data.length(); ++i)
-		{
-			//const size_t current_position = std::distance(Data.begin(), string_iterator);
-			//if (length_case(static_cast<size_t>(current_position + minimal_seeking_length), Data.length() + 1))
-			if (length_case(static_cast<size_t>(i + minimal_seeking_length), Data.length() + 1))
-			{
-				sub_string = Data.substr(i, minimal_seeking_length);
 				Occurences[sub_string].insert(i);
-				//if (visitied.find(sub_string) == visitied.end())
-				//{
-				//	visitied.insert(sub_string);
-				//	//for (typename std::string::const_iterator string_iterator_third = string_iterator; string_iterator_third != Data.end(); ++string_iterator_third)
-				//	for (size_t j = i; j < Data.length(); ++j)
-				//	{
-				//		//const size_t second_current_position = std::distance(Data.begin(), string_iterator_third);
-				//		//if (length_case(static_cast<size_t>(second_current_position + minimal_seeking_length), Data.length() + 1))
-				//		if (length_case(static_cast<size_t>(j + minimal_seeking_length), Data.length() + 1))
-				//		{
-				//			second_sub_string = Data.substr(j, minimal_seeking_length);
-				//			if (second_sub_string == sub_string)
-				//			{
-				//				//Temp_Second.emplace_back(std::make_pair(sub_string, second_current_position));
-				//				Temp_Second.emplace_back(std::make_pair(sub_string, j));
-				//			}
-				//		}
-				//		else
-				//		{
-				//			if (Temp_Second.size() > 1)
-				//			{
-				//				Temp.emplace_back(Temp_Second);
-				//			}		
-				//			Temp_Second.clear();
-				//			break;
-				//		}
-				//	}
-				//}
+//				std::cout << sub_string << '\n';
 			}
-			else
-			{
-				break;
-			}			
 		}
+		//std::cin.get();
 		++minimal_seeking_length;
 	}
 
 	std::vector<int32_t> Differences{};
 	int tab[19]{};
 
-	//for (typename std::vector<std::vector<std::pair<std::string, size_t>>>::const_iterator vec_iterator = Temp.begin(); vec_iterator != Temp.end(); ++vec_iterator)
-	//{
-	//	for (typename std::vector<std::pair<std::string, size_t>>::const_iterator vec_iterator_second = vec_iterator->begin(); vec_iterator_second != (vec_iterator->end() - 1); ++vec_iterator_second)
-	//	{
-	//		Differences.emplace_back((vec_iterator_second + 1)->second - vec_iterator->begin()->second);
-	//		std::vector<int32_t>::const_iterator vec_iterator = (Differences.end() - 1);
-	//		for (size_t i = 2; i <= 20; ++i)
-	//		{
-	//			if (*vec_iterator % i == 0)
-	//			{
-	//				tab[i - 2]++;
-	//			}
-	//		}
-	//	}
-	//}
-
 	for (typename std::map<std::string, std::set<int32_t>>::const_iterator map_iterator = Occurences.begin(); map_iterator != Occurences.end(); ++map_iterator)
 	{
-		//Differences.emplace_back((vec_iterator_second + 1)->second - vec_iterator->begin()->second);
-		//Differences.emplace_back((map_iterator + 1)->second - map_iterator->begin()->second);
 		if (map_iterator->second.size() > 1)
 		{
 			//std::cout << map_iterator->first << " : ";
-			for (typename std::set<int32_t>::const_iterator set_iterator = map_iterator->second.begin(); set_iterator != std::prev(map_iterator->second.end()); ++set_iterator)
+			for (typename std::set<int32_t>::const_iterator set_iterator = map_iterator->second.begin(); set_iterator != map_iterator->second.end(); ++set_iterator)
 			{
 				//std::cout << *set_iterator << ' ';
+				//int32_t temporary{ *map_iterator->second.begin() };
 				Differences.emplace_back((*set_iterator) - *map_iterator->second.begin());
+				
 			}
 			//std::cout << '\n';
 		}
@@ -238,10 +162,16 @@ void Find_Occurences(const std::string& Data, size_t minimal_seeking_length)
 	}
 	//system("pause");
 
-	//for (typename std::vector<int32_t>::const_iterator vec_iterator = Differences.begin(); vec_iterator != Differences.end(); ++vec_iterator)
-	//{
-	//	
-	//}
+	for (typename std::vector<int32_t>::const_iterator vec_iterator = Differences.begin(); vec_iterator != Differences.end(); ++vec_iterator)
+	{
+		for (size_t i = 2; i <= 20; ++i)
+		{
+			if (*(vec_iterator) % i == 0)
+			{
+				tab[i - 2]++;
+			}
+		}
+	}
 
 	for (size_t i = 0; i < 19; ++i)
 	{
