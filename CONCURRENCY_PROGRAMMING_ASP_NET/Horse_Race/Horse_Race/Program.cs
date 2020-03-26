a2248a04bbf1467f85a45470f2e7e0fd86211847
Damian Andrysiak <andrydamian@gmail.com>
2020-03-26 13:00:03 +0100

    up C# concurrecy

---
+++ CONCURRENCY_PROGRAMMING_ASP_NET/Horse_Race/Horse_Race/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+namespace Horse_Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sluzewiec Obj = new Sluzewiec(300);
+            //Obj.Print();
+            Obj.Start_Race();
+        }
+    }
+}
