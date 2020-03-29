80191f530c66869f1977c2962020eba8bef8618a
Damian Andrysiak <andrydamian@gmail.com>
2020-03-30 01:35:34 +0200

    up

--- CONCURRENCY_PROGRAMMING_ASP_NET/Horse_Race/Temp_Horse_Race/Program.cs
+++ CONCURRENCY_PROGRAMMING_ASP_NET/Horse_Race/Temp_Horse_Race/Program.cs
@@ -3,13 +3,11 @@
 namespace Temp_Horse_Race
 {
 
-
-
     class Program
     {
         static void Main(string[] args)
         {
-            Sluzewiec Sluzewiec = new Sluzewiec(15);
+            Sluzewiec Sluzewiec = new Sluzewiec(40);
             Sluzewiec.Start_Race();
             Sluzewiec.Print_Results();
         }
