f7a6a0958cfa387aa343214cf707a133f7c4ddf3
Damian Andrysiak <andrydamian@gmail.com>
2020-04-01 20:40:15 +0200

    up

--- ALGORITHMS/Huffman_Coding/Huffman_Coding/main.cpp
+++ ALGORITHMS/Huffman_Coding/Huffman_Coding/main.cpp
@@ -237,9 +237,88 @@ void Huffman_Coding::Print_Occurencies() const
 	}
 }
 
+struct bintree_node {
+	bintree_node* left;
+	bintree_node* right;
+	int data;
+};
+class bst
+{
+	bintree_node* root;
+public:
+	bst() 
+	{
+		root = NULL;
+	}
+	int isempty()
+	{
+		return(root == NULL);
+	}
+	void insert(int item);
+	void displayBinTree();
+	void printBinTree(bintree_node*);
+
+	~bst();
+};
+
+void bst::insert(int item)
+{
+	bintree_node* p = new bintree_node;
+	bintree_node* parent;
+	p->data = item;
+	p->left = NULL;
+	p->right = NULL;
+	parent = NULL;
+	if (isempty())
+	{
+		root = p;
+	}
+	else 
+	{
+		bintree_node* ptr;
+		ptr = root;
+		while (ptr != NULL)
+		{
+
+			parent = ptr;
+			if (item > ptr->data)
+			{
+				ptr = ptr->right;
+			}
+			else
+			{
+				ptr = ptr->left;
+			}
+		}
+		if (item < parent->data)
+		{
+			parent->left = p;
+		}
+		else
+		{
+			parent->right = p;
+		}
+	}
+}
+void bst::displayBinTree()
+{
+	printBinTree(root);
+}
+void bst::printBinTree(bintree_node* ptr)
+{
+	if (ptr != NULL) 
+	{
+		printBinTree(ptr->left);
+		std::cout << "  " << ptr->data << "     ";
+		printBinTree(ptr->right);
+	}
+}
+
 void Huffman_Coding::Create_Tree()
 {
-	std::vector<std::pair<int32_t, std::pair<bool, bool>>> Tree{};
+	//std::vector<std::pair<int32_t, std::pair<bool, bool>>> Tree{};
+	std::vector<int32_t> Tree{};
+	//std::vector<Node> Tree{};
 
 	//auto the_smallest_in_tree = [&](const typename std::vector<int32_t>::const_iterator current_pos)
 	//{
@@ -268,9 +347,12 @@ void Huffman_Coding::Create_Tree()
 				{
 					//false means it is not a node
 					//true means it is a node
-					Tree.emplace_back(std::make_pair(map_copy[i].second, std::make_pair(0,false)));
-					Tree.emplace_back(std::make_pair(map_copy[i + 1].second, std::make_pair(1, false)));
-					Tree.emplace_back(std::make_pair(map_copy[i].second + map_copy[i + 1].second, std::make_pair(0, true)));
+					Tree.emplace_back(map_copy[i].second);
+					Tree.emplace_back(map_copy[i + 1].second);
+					Tree.emplace_back(map_copy[i].second + map_copy[i + 1].second);
+					//Tree.emplace_back(std::make_pair(map_copy[i].second, std::make_pair(0,false)));
+					//Tree.emplace_back(std::make_pair(map_copy[i + 1].second, std::make_pair(1, false)));
+					//Tree.emplace_back(std::make_pair(map_copy[i].second + map_copy[i + 1].second, std::make_pair(0, true)));
 					i = std::distance(this->map_copy.begin(), (vec_iterator + 2));
 					break;
 				}
@@ -278,18 +360,23 @@ void Huffman_Coding::Create_Tree()
 				{
 					if (i + 1 < map_copy.size())
 					{
-						if ((Tree.end() - 1)->first <= map_copy[i + 1].second)
+						if (*(Tree.end() - 1) <= map_copy[i + 1].second)
 						{
-							Tree.emplace_back(std::make_pair(map_copy[i].second, std::make_pair(1, false)));
-							Tree.emplace_back(std::make_pair(map_copy[i].second + (Tree.end() - 2)->first, std::make_pair(0, true)));
+							//Tree.emplace_back(std::make_pair(map_copy[i].second, std::make_pair(1, false)));
+							//Tree.emplace_back(std::make_pair(map_copy[i].second + (Tree.end() - 2)->first, std::make_pair(0, true)));
+							Tree.emplace_back(map_copy[i].second);
+							Tree.emplace_back(map_copy[i].second + *(Tree.end() - 2));
 							i = std::distance(this->map_copy.begin(), (vec_iterator + 2));
 							break;
 						}
 						else
 						{
-							Tree.emplace_back(std::make_pair(map_copy[i].second, std::make_pair(1, false)));
-							Tree.emplace_back(std::make_pair(map_copy[i + 1].second, std::make_pair(0, false)));
-							Tree.emplace_back(std::make_pair(map_copy[i].second + map_copy[i + 1].second, std::make_pair(1, false)));
+							//Tree.emplace_back(std::make_pair(map_copy[i].second, std::make_pair(1, false)));
+							//Tree.emplace_back(std::make_pair(map_copy[i + 1].second, std::make_pair(0, false)));
+							//Tree.emplace_back(std::make_pair(map_copy[i].second + map_copy[i + 1].second, std::make_pair(1, false)));
+							Tree.emplace_back(map_copy[i].second);
+							Tree.emplace_back(map_copy[i + 1].second);
+							Tree.emplace_back(map_copy[i].second + map_copy[i + 1].second);
 							i = std::distance(this->map_copy.begin(), (vec_iterator + 2));
 							break;
 						}
@@ -300,52 +387,55 @@ void Huffman_Coding::Create_Tree()
 						//merge array finally
 						//std::cin.get();
 						if_break = true;
-						//Tree.emplace_back(std::make_pair(20,std::make_pair(0, true)));
+						Tree.emplace_back(20);
 						break;
 					}
 				}
 			}
 		}
 
+		for (size_t i = 0; i < Tree.size(); ++i)
+		{
 
+		}
 		////Tree.emplace_back(std::make_pair((Tree.end() - 1)->first + (Tree.end() - 2)->first, true));
-		//for (typename std::vector<std::pair<int32_t, bool>>::const_iterator vec_iterator = Tree.begin(); vec_iterator != Tree.end(); ++vec_iterator)
-		//{
-		//	std::cout << vec_iterator->first << ' ' << std::boolalpha << vec_iterator->second << '\n';
-		//}
+		for (typename std::vector<int32_t>::const_iterator vec_iterator = Tree.begin(); vec_iterator != Tree.end(); ++vec_iterator)
+		{
+			std::cout << *vec_iterator << '\n';
+		}
 		//Tree.emplace_back(std::make_pair((Tree.end() - 1)->first + (Tree.end() - 2)->first, true));
 		//for (typename std::vector<std::pair<int32_t, std::pair<bool, bool>>>::const_iterator vec_iterator = Tree.begin(); vec_iterator != Tree.end(); ++vec_iterator)
 		//{
 		//	std::cout << vec_iterator->first << ' ' << std::boolalpha << vec_iterator->second.second << '\n';
 		//}
 		//std::cin.get();
-		size_t counter{};
-		for (size_t i = 0; i < Tree.size(); ++i)
-		{
-			if (Tree[i].second.second == false)
-			{
-				std::cout << map_copy[counter].first << " ";
-				counter++;
-				for (size_t j = i; j < Tree.size(); ++j)
-				{
-					if (Tree[j].second.second == true)
-					{
-						if (Tree[j].second.first == 0)
-						{
-							std::cout << "0";
-						}
-						else
-						{
-							std::cout << "1";
-						}
-					}
+		//size_t counter{};
+		//for (size_t i = 0; i < Tree.size(); ++i)
+		//{
+		//	if (Tree[i].second.second == false)
+		//	{
+		//		std::cout << map_copy[counter].first << " ";
+		//		counter++;
+		//		for (size_t j = i; j < Tree.size(); ++j)
+		//		{
+		//			if (Tree[j].second.second == true)
+		//			{
+		//				if (Tree[j].second.first == 0)
+		//				{
+		//					std::cout << "0";
+		//				}
+		//				else
+		//				{
+		//					std::cout << "1";
+		//				}
+		//			}
 
 
-				}
-				std::cout << "\n";
-			}
+		//		}
+		//		std::cout << "\n";
+		//	}
 
-		}
+		//}
 		std::cin.get();
 
 
@@ -361,4 +451,8 @@ void Huffman_Coding::Create_Tree()
 Huffman_Coding::~Huffman_Coding()
 {
 
+}
+
+bst::~bst()
+{
 }
