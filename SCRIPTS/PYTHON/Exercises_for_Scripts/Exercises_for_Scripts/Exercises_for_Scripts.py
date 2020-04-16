2493253e960761daef73661450006b7dcbc67029
Damian Andrysiak <andrydamian@gmail.com>
2020-04-16 15:15:55 +0200

    up

--- SCRIPTS/PYTHON/Exercises_for_Scripts/Exercises_for_Scripts/Exercises_for_Scripts.py
+++ SCRIPTS/PYTHON/Exercises_for_Scripts/Exercises_for_Scripts/Exercises_for_Scripts.py
@@ -1,6 +1,7 @@
 import locale
 import calendar
 from sortedcontainers import SortedList
+from datetime import date
 
 #Zadanie 1
 
@@ -79,6 +80,64 @@ from sortedcontainers import SortedList
 #    if number_length == 9:
 #        print(number)
 
+
 ###########################################################################
 
 #Zadanie4
+
+#def int_to_roman(number):
+#    numerals={1:"I", 4:"IV", 5:"V", 9: "IX", 10:"X", 40:"XL", 50:"L",
+#              90:"XC", 100:"C", 400:"CD", 500:"D", 900:"CM", 1000:"M"}
+#    result=""
+#    number = int(number,base=10)
+#    for value, numeral in sorted(numerals.items(), reverse=True):
+#        while number >= value:
+#            result += numeral
+#            number -= value
+#    return result
+    
+#print("W systemie rzymskim to:", int_to_roman(input("Wpisz calkowita liczbe: ")))
+
+
+###########################################################################
+
+#Zadanie5
+
+#def Get_Decimal(str):
+#    day = int()
+#    month = int()
+#    year = int()
+#    counter = 0
+#    temp_str = ""
+#    for char in str:
+#        if char == '.':
+#            if counter == 0:
+#                day = int(temp_str,base=10)
+#            elif counter == 1:
+#                month = int(temp_str,base=10)
+#            counter += 1
+#            temp_str = ""
+#        else:
+#            temp_str += char
+#    year = int(temp_str,base=10)
+
+#    return date(year, month, day)
+
+
+#print("Podaj pierwsza date $ ")
+#str_date_1 = input()
+
+#print("Podaj druga date $ ")
+#str_date_2 = input()
+
+
+#f_date = Get_Decimal(str_date_1)
+#l_date = Get_Decimal(str_date_2)
+#delta = l_date - f_date
+#print("Te dwie daty dzieli: " , delta.days , " Dni")
+
+
+###########################################################################
+
+#Zadanie6
+
