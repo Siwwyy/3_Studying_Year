b968c588feb81b579c117a902cab8f23c9eeb65e
Damian Andrysiak <andrydamian@gmail.com>
2020-04-18 00:13:32 +0200

    up

--- GUI_PROGRAMMING/SIXTH/Julia/mainwindow.cpp
+++ GUI_PROGRAMMING/SIXTH/Julia/mainwindow.cpp
@@ -13,3 +13,82 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::paintEvent(QPaintEvent *e)
+{
+    QPainter painter(this);
+
+
+//    QPolygon poly;
+
+//    poly << QPoint(10,10);
+//    poly << QPoint(10,100);
+//    poly << QPoint(100,10);
+//    poly << QPoint(100,100);
+
+
+
+    //Draw Something
+    for(size_t i = 0; i < QWidget::width(); ++i)
+    {
+        for(size_t j = 0; j < QWidget::height(); ++j)
+        {
+//            int juliaValue = Julia( i,j );
+//            int offset = i + j * QWidget::height();
+//            QColor color(255*juliaValue,0,0,255);
+
+            int mangelbotvalue = MangelBot( i,j );
+//            int offset = i + j * QWidget::height();
+            QColor color(255*mangelbotvalue,0,0,255);
+
+            QPen linepen;
+            linepen.setWidth(8);
+            linepen.setColor(color);
+            painter.setPen(linepen);
+            painter.drawPoint(QPoint(i,j));
+        }
+    }
+
+}
+
+
+int MainWindow::Julia(int x, int y)
+{
+    const float scale = 1.5;
+    float jx = scale * (float)(QWidget::width()/2 - x)/(QWidget::width()/2);
+    float jy = scale * (float)(QWidget::width()/2 - y)/(QWidget::width()/2);
+
+    Complex c(-0.8, 0.156);
+    Complex a(jx, jy);
+
+    for (size_t i=0; i<200; i++)
+    {
+        a = a * a + c;
+        if (a.magnitude2() > 1000)
+         {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int MainWindow::MangelBot(int x, int y)
+{
+    Complex point((float)x/QWidget::width()-1.5, (float)y/QWidget::height()-0.5);
+
+    Complex z(0, 0);
+    unsigned int nb_iter = 0;
+    while (qFabs(z.r) < 2 && nb_iter <= 34)
+    {
+       z = z * z + point;
+       nb_iter++;
+    }
+    if (nb_iter < 34)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
--- GUI_PROGRAMMING/SIXTH/Julia/mainwindow.h
+++ GUI_PROGRAMMING/SIXTH/Julia/mainwindow.h
@@ -2,10 +2,49 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtGui>
+#include <QtCore>
+#include <QtMath>
+#include <complex>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+
+namespace Ui
+{
+
+class MainWindow;
+
+}
+
 QT_END_NAMESPACE
+
+struct Complex
+{
+    float   r;
+    float   i;
+
+    Complex( float a, float b )
+        : r(a), i(b)
+    {
+
+    }
+
+    float magnitude2( void )
+    {
+        return r * r + i * i;
+    }
+
+    Complex operator*(const Complex& a)
+    {
+        return Complex(r*a.r - i*a.i, i*a.r + r*a.i);
+    }
+
+    Complex operator+(const Complex& a)
+    {
+        return Complex(r+a.r, i+a.i);
+    }
+};
+
 
 class MainWindow : public QMainWindow
 {
@@ -17,5 +56,9 @@ public:
 
 private:
     Ui::MainWindow *ui;
+protected:
+    void paintEvent(QPaintEvent * e);
+    int Julia(int x, int y);
+    int MangelBot(int x, int y);
 };
 #endif // MAINWINDOW_H
