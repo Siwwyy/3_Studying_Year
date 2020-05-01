34493cbf82550297fb891d7752d533f85cf6d4fb
Damian Andrysiak <andrydamian@gmail.com>
2020-05-01 12:13:29 +0200

    up

--- DATA_PROTECTION/Diffi_Hellman_Protocol/Diffi_Hellman_Protocol/main.cpp
+++ DATA_PROTECTION/Diffi_Hellman_Protocol/Diffi_Hellman_Protocol/main.cpp
@@ -7,6 +7,7 @@
 
 
 const int32_t powmod(const int32_t a, const int32_t b, const uint32_t mod);
+bool Is_Prime(const int32_t a);
 
 int main(int argc, char* argv[])
 {
@@ -31,45 +32,38 @@ int main(int argc, char* argv[])
 
 
 
-	int32_t p{ 7 };	//check is it a prime number always !
-	int32_t g{ 2 };
+	int32_t p{};	//check is it a prime number always !
+	int32_t g{};
 
 	/*
 		SECRET NUMBERS
 	*/
 
-	int32_t Alicja{ 5 };
-	int32_t Bob{ 4 };
+	int32_t a{};
+	int32_t b{};
 
 	/////////////////////////////////////
 	std::cout << "p=";
 	std::cin >> p;
-	std::cout << "g=";
-	std::cin >> g;
-	std::cout << "a=";
-	std::cin >> Alicja;
+	if (Is_Prime(p) == true)
+	{
+		std::cout << "g=";
+		std::cin >> g;
+		std::cout << "a=";
+		std::cin >> a;
 
-	int32_t A = powmod(g, Alicja, p);	//powmod
+		int32_t A = powmod(g, a, p);	//powmod
+		std::cout << "A=" << A << '\n';
 
-	std::cout << "b=";
-	std::cin >> Bob;
+		int32_t B{};
+		std::cout << "B=";
+		std::cin >> B;
 
-	int32_t B = powmod(g, Bob, p);		//powmod
+		/*int32_t B{}*/; /*= powmod(g, b, p)*/;		//powmod
 
-
-	int32_t k_A{ 1 };
-	int32_t k_B{ };
-
-	//std::cout << A << " " << B;
-	//while (k_A != k_B)
-	//{
-	//	k_A = powmod(B, Alicja, p);
-	//	k_B = powmod(A, Bob, p);
-	//}
-	k_A = powmod(B, Alicja, p);
-	k_B = powmod(A, Bob, p);
-
-	std::cout << k_A << " " << k_B << '\n';
+		int32_t k = powmod(B, a, p);
+		std::cout << "k=" << k << '\n';
+	}
 
 	system("pause");
 	return EXIT_SUCCESS;
@@ -95,4 +89,21 @@ const int32_t powmod(const int32_t a, const int32_t b, const uint32_t mod)
 	}
 
 	return result;
+}
+
+bool Is_Prime(const int32_t a)
+{
+	int32_t root = sqrt(a);
+	bool is_prime = true;
+
+	for (size_t i = 2; i <= root; ++i) 
+	{
+		if (a % i == 0) 
+		{
+			is_prime = false;
+			break;
+		}
+	}
+
+	return is_prime;
 }
