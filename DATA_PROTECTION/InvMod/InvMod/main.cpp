5e50fdab508cdb6a9bb15d7be286ea74455d0e64
Damian Andrysiak <andrydamian@gmail.com>
2020-05-10 19:06:36 +0200

    working inverse euclidean modulo

--- DATA_PROTECTION/InvMod/InvMod/main.cpp
+++ DATA_PROTECTION/InvMod/InvMod/main.cpp
@@ -7,15 +7,14 @@
 
 
 const int32_t NWD(int32_t a, int32_t b);
-const void Inverse_Euclidean(int32_t a, int32_t b);
+const int32_t Inverse_Euclidean(int32_t a, int32_t b);
 
 int main(int argc, char* argv[])
 {
 	//std::cout << NWD(28, 24) << '\n';
-
-	//std::cout << Inverse_Euclidean(7, 10) << '\n';
-	//Inverse_Euclidean(161, 28);
-	Inverse_Euclidean(5, 11);
+	std::cout << Inverse_Euclidean(337, 123) << '\n';
+	std::cout << Inverse_Euclidean(4321, 1234) << '\n';
+	std::cout << Inverse_Euclidean(432, 321) << '\n';
 
 	system("pause");
 	return EXIT_SUCCESS;
@@ -33,7 +32,7 @@ const int32_t NWD(int32_t a, int32_t b)
 	return a;
 }
 
-const void Inverse_Euclidean(int32_t a, int32_t b)
+const int32_t Inverse_Euclidean(int32_t a, int32_t b)
 {
 	int32_t q{}, r1{ a }, r2{ b }, r{}, s1{}, s2{}, s{}, t1{}, t2{}, t{};
 
@@ -49,28 +48,14 @@ const void Inverse_Euclidean(int32_t a, int32_t b)
 		r = r1 % r2;
 		s = s1 - s2 * q;
 		t = t1 - t2 * q;
-		std::cout << "q = " << q << '\n';
-		std::cout << "r1 = " << r1 << '\n';
-		std::cout << "r2 = " << r2 << '\n';
-		std::cout << "r = " << r << '\n';
-		std::cout << "s1 = " << s1 << '\n';
-		std::cout << "s2 = " << s2 << '\n';
-		std::cout << "s = " << s << '\n';
-		std::cout << "t1 = " << t1 << '\n';
-		std::cout << "t2 = " << t2 << '\n';
-		std::cout << "t = " << t << '\n';
-		std::cout << "-------------------------------------\n";
-		//std::cout << q << ' ' << r1 << ' ' << r2 << ' ' << r << ' ' << s1 << ' ' << s2 << ' ' << s << ' ' << t1 << ' ' << t2 << ' ' << t << '\n';
-
-		//int32_t temp_q{q}, temp_r1{ r1 }, temp_r2{ r2 }, temp_r{r}, temp_s1{s1}, temp_s2{s2}, temp_s{s}, temp_t1{t1}, temp_t2{t2}, temp_t{t};
 		if (r == 0)
 		{
 			r1 = r2;
 			r2 = r;
 			s1 = s2;
-			s2 = s;
+			s = s2;
 			t1 = t2;
-			t2 = t;
+			t = t2;
 			break;
 		}
 		else if (r == 1)
@@ -83,81 +68,29 @@ const void Inverse_Euclidean(int32_t a, int32_t b)
 		s2 = s;
 		t1 = t2;
 		t2 = t;
-
-
-
-
 	}
-	//std::cout << q << ' ' << r1 << ' ' << r2 << ' ' << r << ' ' << s1 << ' ' << s2 << ' ' << s << ' ' << t1 << ' ' << t2 << ' ' << t << '\n';
-	//std::cout << '\n';
-	std::cout << "q = " << q << '\n';
-	std::cout << "r1 = " << r1 << '\n';
-	std::cout << "r2 = " << r2 << '\n';
-	std::cout << "r = " << r << '\n';
-	std::cout << "s1 = " << s1 << '\n';
-	std::cout << "s2 = " << s2 << '\n';
-	std::cout << "s = " << s << '\n';
-	std::cout << "t1 = " << t1 << '\n';
-	std::cout << "t2 = " << t2 << '\n';
-	std::cout << "t = " << t << '\n';
-	std::cout << "-------------------------------------\n";
 	if (s < 0)
 	{
 		s += b;
 	}
-	std::cout << r1 << " " << s << " " << t << '\n';
-	std::cout << '\n';
 	//s*a+t*b = gcd(a,b)
 	/*
+		my sample
 		GCD = 7
 		s = -1
 		t = 6
 
 		s*a + t*b => (-1)*161+6*28 = -161 + 168 = 7 = GCD
 	*/
-
-	int32_t cos = (s1 * a + t1 * b);
-	int32_t result = (s1 * a + t1 * b) % b;
-
-
-	std::cout << result;
-	std::cout << '\n';
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	//std::cout << r1 << " " << s1 << " " << t1 << '\n';
-
-	//int u, w, x, z, q;
-
-	//u = 1; w = a;
-	//x = 0; z = b;
-	//while (w)
-	//{
-	//	if (w < z)
-	//	{
-	//		q = u; u = x; x = q;
-	//		q = w; w = z; z = q;
-	//	}
-	//	q = w / z;
-	//	u -= q * x;
-	//	w -= q * z;
-	//}
-	//if (z == 1)
-	//{
-	//	if (x < 0) x += b;
-	//	std::cout << x << std::endl;
-	//}
-	//else std::cout << "BRAK\n";
+	int32_t result = (a*s)%b;
+	if (result == 1)
+	{
+		std::cout << "Istnieje ";
+		return s;
+	}
+	else
+	{
+		std::cout << "Nie istnieje ";
+		return -1;
+	}
 }
