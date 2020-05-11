484b131aa66b64be3ed5ffbda9537dc73c9005c8
Damian Andrysiak <andrydamian@gmail.com>
2020-05-11 13:02:30 +0200

    up

---
+++ DATA_PROTECTION/RSA/RSA/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdint>
+#include <cstdlib>
+#include <bitset>
+#include <cmath>
+
+
+int main(int argc, char* argv[])
+{
+
+	system("pause");
+	return EXIT_SUCCESS;
+}
