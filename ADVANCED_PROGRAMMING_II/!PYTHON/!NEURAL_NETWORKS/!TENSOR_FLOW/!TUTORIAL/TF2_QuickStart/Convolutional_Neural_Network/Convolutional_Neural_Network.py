c88ae88bdfe1c6df3bb94f1e26e6deb8cb347e1d
Damian Andrysiak <andrydamian@gmail.com>
2020-05-15 12:00:21 +0200

    up, convolutional neural network

--- ADVANCED_PROGRAMMING_II/!PYTHON/!NEURAL_NETWORKS/!TENSOR_FLOW/!TUTORIAL/TF2_QuickStart/Convolutional_Neural_Network/Convolutional_Neural_Network.py
+++ ADVANCED_PROGRAMMING_II/!PYTHON/!NEURAL_NETWORKS/!TENSOR_FLOW/!TUTORIAL/TF2_QuickStart/Convolutional_Neural_Network/Convolutional_Neural_Network.py
@@ -7,4 +7,32 @@ import pickle
 X = pickle.load(open("X.pickle", "rb"))
 Y = pickle.load(open("Y.pickle", "rb"))
 
+X = X/255.0
 
+
+# Neural network
+model = Sequential()
+
+#First Layer
+model.add(  Conv2D(64, (3,3), input_shape = X.shape[1:])    )
+model.add(  Activation("relu")  )
+model.add(  MaxPooling2D(pool_size=(2,2))    )
+
+#Second Layer
+model.add(  Conv2D(64, (3,3))   )
+model.add(  Activation("relu")  )
+model.add(  MaxPooling2D(pool_size=(2,2))    )
+
+#Thrid Layer
+#model.add(Flatten(data_format=(3,3)))
+model.add(  Flatten()   )
+model.add(  Dense(64)   )
+
+#Output Layer
+model.add(  Dense(1)    )
+model.add(  Activation("sigmoid")   )
+
+
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=['accuracy'])
+
+model.fit(X,Y, batch_size=32, epochs=3,validation_split=0.1,verbose=1)
