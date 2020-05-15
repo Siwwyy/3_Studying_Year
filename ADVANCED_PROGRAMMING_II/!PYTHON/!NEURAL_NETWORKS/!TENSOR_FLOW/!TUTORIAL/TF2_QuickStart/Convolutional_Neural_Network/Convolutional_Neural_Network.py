7920745ad9e879a53205cf1219d752d893e9fd68
Damian Andrysiak <andrydamian@gmail.com>
2020-05-15 12:45:40 +0200

    added logs for convolutional network

--- ADVANCED_PROGRAMMING_II/!PYTHON/!NEURAL_NETWORKS/!TENSOR_FLOW/!TUTORIAL/TF2_QuickStart/Convolutional_Neural_Network/Convolutional_Neural_Network.py
+++ ADVANCED_PROGRAMMING_II/!PYTHON/!NEURAL_NETWORKS/!TENSOR_FLOW/!TUTORIAL/TF2_QuickStart/Convolutional_Neural_Network/Convolutional_Neural_Network.py
@@ -1,8 +1,23 @@
 
 import tensorflow as tf
+from tensorflow import keras
+from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+from keras.callbacks import TensorBoard
 import pickle
+import time
+
+#gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
+#sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+
+#NAME = "A-vs-B-sign_alphabet-cnn-64x2-{}".format(int(time.time()))
+#NAME = "what{}".format(int(time.time()))
+
+#My_TensorBoard = TensorBoard(log_dir="logs/".format(NAME))
+tensorboard_callback = TensorBoard(log_dir=".\\logs")
+
 
 X = pickle.load(open("X.pickle", "rb"))
 Y = pickle.load(open("Y.pickle", "rb"))
@@ -26,7 +41,6 @@ model.add(  MaxPooling2D(pool_size=(2,2))    )
 #Thrid Layer
 #model.add(Flatten(data_format=(3,3)))
 model.add(  Flatten()   )
-model.add(  Dense(64)   )
 
 #Output Layer
 model.add(  Dense(1)    )
@@ -35,4 +49,5 @@ model.add(  Activation("sigmoid")   )
 
 model.compile(loss="binary_crossentropy", optimizer="adam", metrics=['accuracy'])
 
-model.fit(X,Y, batch_size=32, epochs=3,validation_split=0.1,verbose=1)
+
+model.fit(X,Y, batch_size=32, epochs=20,validation_split=0.3,verbose=1,callbacks=[tensorboard_callback])
