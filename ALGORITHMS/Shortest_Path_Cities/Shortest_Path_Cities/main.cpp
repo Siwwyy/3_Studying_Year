1440a0dabae46abf5f71461cc94f04a78b74ffc6
Damian Andrysiak <andrydamian@gmail.com>
2020-05-21 23:23:24 +0200

    work counting cities distance, given in kilometers

--- ALGORITHMS/Shortest_Path_Cities/Shortest_Path_Cities/main.cpp
+++ ALGORITHMS/Shortest_Path_Cities/Shortest_Path_Cities/main.cpp
@@ -2,8 +2,10 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <cmath>
 #include "Djikstra.hpp"
 
+#define M_PI 3.14159265358979323846
 
 const _STD vector<int32_t>& Get_File_Data(const std::string& file_path);
 const bool Invalid_Characters(const char character);
@@ -39,7 +41,9 @@ const::std::vector<int32_t>& Get_File_Data(const std::string& file_path)
 	std::vector<std::string> Temp_Data{};
 
 	std::vector<std::string> Cities{};
-	std::vector<std::pair<std::pair<int32_t, int32_t>, std::pair<int32_t, int32_t>>> Coords{};
+	/*std::vector<std::pair<std::pair<int32_t, int32_t>, std::pair<int32_t, int32_t>>> Coords{};*/
+
+	std::vector<std::pair<float, float>> Coords{};
 
 	std::fstream file_in{};
 	file_in.open(file_path.c_str(), std::ios_base::in | std::ios_base::binary);
@@ -75,8 +79,10 @@ const::std::vector<int32_t>& Get_File_Data(const std::string& file_path)
 		//std::cout << Temp_Data[i] << '\n';
 		std::string city_name{};
 		std::string single_coord{};
-		int32_t coords[4]{};
+		int32_t coords[2]{};
 		size_t counter{};
+		float x{};
+		float y{};
 		for (size_t j = 0; j < Temp_Data[i].size(); ++j)
 		{
 			//Inserting city name
@@ -95,13 +101,20 @@ const::std::vector<int32_t>& Get_File_Data(const std::string& file_path)
 					coords[counter] = std::stoi(single_coord);
 					single_coord.clear();
 					++counter;
+					if (counter % 2 == 0 && x == 0.f)
+					{
+						x = static_cast<float>(static_cast<float>(coords[1]) / 60.f) + static_cast<float>(coords[0]);
+						counter = 0;
+					}
 				}
 			}
 		}
 		//Inserting city names
 		Cities.emplace_back(city_name);
 		//Inserting coordinates
-		Coords.emplace_back(std::make_pair(std::make_pair(coords[0], coords[1]), std::make_pair(coords[2], coords[3])));
+		y = static_cast<float>(static_cast<float>(coords[1]) / 60.f) + static_cast<float>(coords[0]);
+		Coords.emplace_back(std::make_pair(x, y));
+		//Coords.emplace_back(std::make_pair(std::make_pair(coords[0], coords[1]), std::make_pair(coords[2], coords[3])));
 	}
 
 	//for (size_t i = 0; i < Cities.size(); ++i)	//works
@@ -109,10 +122,60 @@ const::std::vector<int32_t>& Get_File_Data(const std::string& file_path)
 	//	std::cout << Cities[i] << '\n';
 	//}
 
+	//for (size_t i = 0; i < Coords.size(); ++i)	//works
+	//{
+	//	std::cout << Cities[i] << "  | x1:" << Coords[i].first.first << " y1:" << Coords[i].first.second << " | x2:" << Coords[i].second.first << " y2:" << Coords[i].second.second << '\n';
+	//}
+
+
 	for (size_t i = 0; i < Coords.size(); ++i)	//works
 	{
-		std::cout << Cities[i] << "  | x1:" << Coords[i].first.first << " y1:" << Coords[i].first.second << " | x2:" << Coords[i].second.first << " y2:" << Coords[i].second.second << '\n';
+		for (size_t j = 0; j < Coords.size(); ++j)
+		{
+			float distance{};
+			if (i == j)
+			{
+				Data.emplace_back(0);
+				continue;
+			}
+			else
+			{
+				float x1{ Coords[i].second };
+				float y1{ Coords[i].first };
+				float x2{ Coords[j].second };
+				float y2{ Coords[j].first };
+
+				distance = static_cast<float>(sqrt(pow((x2 - x1), 2) + pow(cos((x1 * M_PI) / 180) * (y2 - y1), 2))) * static_cast<float>(40075.704f / 360.f);
+				Data.emplace_back(static_cast<int32_t>(distance));
+			}
+		}
 	}
+	size_t cities_counter{};
+	for (size_t i = 0; i < Data.size(); ++i)	//works
+	{
+		//if (i == 0)
+		//{
+		//	for (size_t j = 0; j < Cities.size(); ++j)	//works
+		//	{
+		//		std::cout << Cities[j][0] << Cities[j][1] << Cities[j][2] << ' ';
+		//	}
+		//	std::cout << '\n';
+		//}
+		if (i == 0)
+		{
+			std::cout << Cities[cities_counter] << "	 ";
+			++cities_counter;
+		}
+		if (i % Coords.size() == 0 && i > 0)
+		{
+			std::cout << '\n' << Cities[cities_counter] << "	  ";
+			++cities_counter;
+		}
+
+		std::cout << Data[i] << ' ';
+
+	}
+	std::cout << '\n';
 
 
 	return Data;
