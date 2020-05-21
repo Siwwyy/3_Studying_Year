8ab912e1f06756b612d640157b1b659427bf4e34
Damian Andrysiak <andrydamian@gmail.com>
2020-05-21 20:51:52 +0200

    getting cords from current.in file

--- ALGORITHMS/Shortest_Path_Cities/Shortest_Path_Cities/main.cpp
+++ ALGORITHMS/Shortest_Path_Cities/Shortest_Path_Cities/main.cpp
@@ -37,7 +37,9 @@ const::std::vector<int32_t>& Get_File_Data(const std::string& file_path)
 	std::vector<int32_t> Data{};
 
 	std::vector<std::string> Temp_Data{};
+
 	std::vector<std::string> Cities{};
+	std::vector<std::pair<std::pair<int32_t, int32_t>, std::pair<int32_t, int32_t>>> Coords{};
 
 	std::fstream file_in{};
 	file_in.open(file_path.c_str(), std::ios_base::in | std::ios_base::binary);
@@ -72,18 +74,37 @@ const::std::vector<int32_t>& Get_File_Data(const std::string& file_path)
 	{
 		//std::cout << Temp_Data[i] << '\n';
 		std::string city_name{};
+		std::string single_coord{};
+		int32_t coords[4]{};
+		size_t counter{};
 		for (size_t j = 0; j < Temp_Data[i].size(); ++j)
 		{
+			//Inserting city name
 			if (Temp_Data[i][j] == '(' || Temp_Data[i][j] == ')' || ((static_cast<int32_t>(Temp_Data[i][j]) >= 97 && static_cast<int32_t>(Temp_Data[i][j]) <= 122) || (static_cast<int32_t>(Temp_Data[i][j]) >= 65 && static_cast<int32_t>(Temp_Data[i][j]) <= 90)))
 			{
 				city_name += Temp_Data[i][j];
 			}
-			if (Temp_Data[i][j] == '/')
+			//if (Temp_Data[i][j] == '/')
+			//{
+			//	break;
+			//}
+			/////////////////////////////////////////////////////
+			//Inserting Coords
+			if (static_cast<int32_t>(Temp_Data[i][j]) >= 48 && static_cast<int32_t>(Temp_Data[i][j]) <= 57)
 			{
-				break;
+				single_coord += Temp_Data[i][j];
+				if (single_coord.size() == 2)
+				{
+					coords[counter] = std::stoi(single_coord);
+					single_coord.clear();
+					++counter;
+				}
 			}
 		}
+		//Inserting city names
 		Cities.emplace_back(city_name);
+		//Inserting coordinates
+		Coords.emplace_back(std::make_pair(std::make_pair(coords[0], coords[1]), std::make_pair(coords[2], coords[3])));
 	}
 
 	//for (size_t i = 0; i < Cities.size(); ++i)	//works
@@ -91,7 +112,10 @@ const::std::vector<int32_t>& Get_File_Data(const std::string& file_path)
 	//	std::cout << Cities[i] << '\n';
 	//}
 
-
+	for (size_t i = 0; i < Coords.size(); ++i)	//works
+	{
+		std::cout << Coords[i].first.first << " " << Coords[i].first.second << " " << Coords[i].second.first << " " << Coords[i].second.second << '\n';
+	}
 
 
 	return Data;
